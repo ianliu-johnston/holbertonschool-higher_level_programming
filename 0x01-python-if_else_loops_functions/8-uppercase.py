b12d323da9a70c4b723599f5518aef6f724bb04c
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-03 13:39:26 -0800

    Task 8: Had the same problem as 2 and 3

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,6 +6,6 @@ def uppercase(str):
         if count + 1 == length:
             endpt = "\n"
         if ord(char) in range(ord('a'), ord('z')):
-            print(chr(ord(char) - (ord('a') - ord('A'))), end=endpt)
+            print('{:c}'.format(ord(char) - (ord('a') - ord('A'))), end=endpt)
         else:
             print(char, end=endpt)
