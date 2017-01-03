f16df719173dde3516aa5b2ff5032091332da86c
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-03 04:34:11 -0800

    Task 8: Finished

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+def uppercase(str):
+    length = len(str)
+    i = 0
+    endpt = ""
+    for char in str:
+        i += 1
+        if i == length:
+            endpt = "\n"
+        if ord(char) in range(ord('a'), ord('z')):
+            print(chr(ord(char) - (ord('a') - ord('A'))), end=endpt)
+        else:
+            print(char, end=endpt)
+uppercase("holberton")
+uppercase("Holberton School 98 Battery street")
