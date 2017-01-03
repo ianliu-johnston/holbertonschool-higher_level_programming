65aa5981a4be541ebe1fe75aa499829fd7b1cbae
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-03 01:06:47 -0800

    Task 0: Finished

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number > 0:
+    str = 'is positive'
+elif number == 0: 
+    str = 'is zero'
+else:
+    str = 'is negative'
+print('{:d}'.format(number), str);
