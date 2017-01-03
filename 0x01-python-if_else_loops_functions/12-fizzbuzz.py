ebc4a21f2d5c418591b1f2899a065bb86a153a2b
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-03 12:40:31 -0800

    Task 12: Finished

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            print("Fizz", end="")
+        if i % 5 == 0:
+            print("Buzz", end="")
+        if i % 3 != 0 and i % 5 != 0:
+            print('{:d}'.format(i), end="")
+        print(end=" ")
