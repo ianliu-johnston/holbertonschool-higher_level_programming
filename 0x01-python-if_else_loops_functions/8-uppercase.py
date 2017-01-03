b01b9a1d1d5d7203e462f9b063891f0c197eb3c8
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-03 14:53:57 -0800

    Task 8: Reverted

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,6 +5,7 @@ def uppercase(str):
     for count, char in enumerate(str):
         if count + 1 == length:
             endpt = "\n"
-        if ord(char) in range(ord('a'), ord('z')+1):
-            char = chr(ord(char) - (ord('a') - ord('A')))
-        print(char, end=endpt)
+        if ord(char) in range(ord('a'), ord('z')):
+            print(chr(ord(char) - (ord('a') - ord('A'))), end=endpt)
+        else:
+            print(char, end=endpt)
