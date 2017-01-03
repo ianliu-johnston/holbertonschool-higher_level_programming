1065d0f45ddda07beb2215d7005e315fb4fd6a50
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-02 17:23:54 -0800

    Task 7: Finished

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))
