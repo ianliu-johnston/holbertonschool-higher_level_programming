23c6dc83f2214adf7cf97addad93f59cf8c3e94b
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-03 17:24:40 -0800

    Task 8: Added oneliner

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def uppercase(str):
-    print(''.join([chr(ord(c)-32) if ord(c)in range(97, 123) else c for c in str]))
+    print(''.join([chr(ord(c)-32) if ord(c)in range(97, 123) else c
+                  for c in str]))
