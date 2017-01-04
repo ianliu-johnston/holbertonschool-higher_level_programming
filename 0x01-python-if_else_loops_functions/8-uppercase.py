364978e9a36ae74163cd68727ed1fbaffe140bd2
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-03 17:36:13 -0800

    Task 8: Added format() in place of chr()

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def uppercase(str):
-    print(''.join([chr(ord(c)-32) if ord(c)in range(97, 123) else c
+    print(''.join(['{:c}'.format(ord(c)-32) if ord(c)in range(97, 123) else c
                   for c in str]))
