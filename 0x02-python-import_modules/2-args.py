fba23e7f6eb0661c8edeed32b36c5eef4f73a81c
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-05 12:57:30 -0800

    Task 2: Finished

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+import sys
+argc = len(sys.argv) - 1
+delim = 's:' if argc > 1 else '.' if argc == 0 else ':'
+print('{:d} argument{}'.format(argc, delim))
+for count, arg in enumerate(sys.argv[1:]):
+    print('{:d}: {}'.format(count+1, arg))
+
