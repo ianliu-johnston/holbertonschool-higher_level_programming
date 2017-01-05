5550b619dc105173cc551d56140406e9134fcc18
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-05 00:22:13 -0800

    Task 3: Compressed conditional

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for char in range(ord('a'), ord('z')+1):
-    if chr(char) == 'q' or chr(char) == 'e':
-        continue
-    print('{:c}'.format(char), end="")
+    if chr(char) != 'q' and chr(char) != 'e':
+        print('{:c}'.format(char), end="")
