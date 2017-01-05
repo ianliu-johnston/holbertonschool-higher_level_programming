b19aa0fc6875678847a64d988b2d678f922adb7f
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-05 14:17:16 -0800

    Fixed order of importing modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from sys import argv
 if __name__ == "__main__":
+    from sys import argv
     res = ""
     argc = len(argv) - 1
     delim = 's:\n' if argc > 1 else '.' if argc == 0 else ':\n'
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from sys import argv
 if __name__ == "__main__":
+    from sys import argv
     result = 0
     for arg in argv[1:]:
         result += int(arg)
