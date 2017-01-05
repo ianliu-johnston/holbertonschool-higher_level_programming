3747bfe78c9ec0086b0cd8e693084bc69d1e8382
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-05 12:31:03 -0800

    Task 1: Finished

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+print('{:d} / {:d} = {:d}'.format(a, b, round(div(a, b))))
+
