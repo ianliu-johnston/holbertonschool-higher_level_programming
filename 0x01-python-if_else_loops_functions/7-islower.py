1e0315f0c97ee5ce4a03fb2a00fdd6cfdcdff980
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-05 00:24:29 -0800

    Task 7: Deleted redundancy

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def islower(c):
-    return (True if ord(c) in range(ord('a'), ord('z')+1) else False)
+    return (ord(c) in range(ord('a'), ord('z')+1))
     # return 97 <= ord(c) <= 122
