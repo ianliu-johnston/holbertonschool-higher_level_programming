5db7c33b48e12d23d593e504a309a3a6b689c9e8
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-05 00:23:04 -0800

    Task 7: Added a one liner(commented)that does the same thing.

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def islower(c):
-    return (True if ord(c) in range(ord('a'), ord('z')) else False)
+    return (True if ord(c) in range(ord('a'), ord('z')+1) else False)
+    # return 97 <= ord(c) <= 122
