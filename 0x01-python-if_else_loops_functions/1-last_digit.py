40a773e33242e8e8b869be2b943c40c471762aa9
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-05 00:21:28 -0800

    Task 1: Changed mod of negative number to correct syntax

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastdigit = ((number * -1) % 10) * -1 if number < 0 else number % 10
+lastdigit = number % -10 if number < 0 else number % 10
 if lastdigit > 5:
     result = 'and is greater than 5'
 elif lastdigit == 0:
