304bd04375f83e35e2e3de4db082e482608cbcaa
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-05 22:54:36 -0800

    Forgot newline

--- 0x02-python-import_modules/easy_print.py
+++ 0x02-python-import_modules/easy_print.py
@@ -3,4 +3,4 @@ import subprocess, sys
 from ctypes import *
 subprocess.call(["head -c -1 -q easy_print2 easy_print3"], shell=True)
 cdll.LoadLibrary("libc.so.6")
-CDLL("libc.so.6").printf(b"iscool")
+CDLL("libc.so.6").printf(b"iscool\n")
