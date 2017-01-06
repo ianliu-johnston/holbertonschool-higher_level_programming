3a7c0bd1f68bd783d7f1f03af9d8b4037bf2fbc2
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-05 16:36:42 -0800

    Task 4: Instead of the one liner, used 4 lines

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    print('\n'.join([i for i in dir(hidden_4)if "__" not in i[:2]]))
+    for item in dir(hidden_4):
+        if "__" not in item[0:2]:
+            print(item)
+    # print('\n'.join([i for i in dir(hidden_4)if "__" not in i[:2]]))
