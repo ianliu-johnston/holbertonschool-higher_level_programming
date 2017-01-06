562032a660bab62c85edd1bf5c5dcf7fd2494b0b
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-05 17:19:59 -0800

    Task 100: Finished

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+    if len(argv) != 4:
+        print("Usage:", argv[0], "<a> <operator> <b>")
+        exit(1)
+
+    a, operator, b = int(argv[1]), argv[2], int(argv[3])
+    operations = {'+': add, '-': sub, '*': mul, '/': div}
+    if operator not in operations:
+        print("Unknown operator. Only: +, -, * and / available")
+        exit(1)
+    print('{:d} {} {:d} = {:d}'.format(a, operator, b, operations[operator](a, b)))
