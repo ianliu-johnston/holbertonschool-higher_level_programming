c8fcaef2d40e6fb3c8e9dd271965b0d870f0a5a5
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-05 16:48:59 -0800

    Task 4: Added 3 liner in comments which does the same as the 1 liner.

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,3 +3,6 @@ if __name__ == "__main__":
     import hidden_4
     print(''.join([i + '\n' for i in dir(hidden_4)
                   if "__" not in i[:2]]), end="")
+#        for item in dir(hidden_4):
+#            if "__" not in item[0:2]:
+#                print(item)
