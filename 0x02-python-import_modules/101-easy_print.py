e6b184450a3f8745af292f6de77c008dac697727
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-05 22:17:20 -0800

    Task 101: Deleted block comment

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,7 +1,2 @@
 #!/usr/bin/python3
 import easy_print
-#import subprocess
-#subprocess.call(["echo", "#pythoniscool"])
-#from ctypes import *
-#cdll.LoadLibrary("libc.so.6")
-#CDLL("libc.so.6").printf(b"#pythoniscool\n")
