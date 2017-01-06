a7f82c85bce4cfe1557062d49e133fc2717028f9
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-05 22:13:10 -0800

    Task 101: Done. Maybe not obfuscated enough

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+import cheap_print_trick
+#import subprocess
+#subprocess.call(["echo", "#pythoniscool"])
+#from ctypes import *
+#cdll.LoadLibrary("libc.so.6")
+#CDLL("libc.so.6").printf(b"#pythoniscool\n")
---
+++ 0x02-python-import_modules/cheap_print_trick.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import subprocess
+subprocess.call(["echo -n \"#include <stdio.h>\nint main(void)\" > test.c"], shell=True)
+subprocess.call(["echo -n \"{\nprintf(\\\"#pythoniscool\\\");\nreturn(0);\n}\n\" >> test.c"], shell=True)
+subprocess.call(["gcc", "test.c"])
+subprocess.call(["./a.out"])
+print("")
+subprocess.call(["rm", "a.out"])
+subprocess.call(["rm", "test.c"])
