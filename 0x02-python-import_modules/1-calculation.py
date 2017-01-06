65ba523b7e9272417e6f970e52e561b1f551b1e9
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-05 16:32:56 -0800

    Task 1: Somethign

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -7,4 +7,3 @@ if __name__ == "__main__":
     print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
     print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
     print('{:d} / {:d} = {:d}'.format(a, b, round(div(a, b))))
-
