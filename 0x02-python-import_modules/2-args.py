86f2ce2262ebcc7ef5af92f7dab25b7f1e95b5fa
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-05 16:34:36 -0800

    Task 2: Fixed new line issue

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     res = ""
     argc = len(argv) - 1
-    delim = 's:\n' if argc > 1 else '.' if argc == 0 else ':\n'
+    delim = 's:' if argc > 1 else '.' if argc == 0 else ':'
     for count, arg in enumerate(argv[1:]):
         res += '{:d}: {}\n'.format(count+1, arg)
-    print('{:d} argument{}{}'.format(argc, delim, res), end="")
+    print('{:d} argument{}\n{}'.format(argc, delim, res), end="")
