976f9e64ddd15c1af304ac81674bf521bbb6dd90
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-05 23:12:01 -0800

    More Obfuscation

--- 0x02-python-import_modules/easy_print.py
+++ 0x02-python-import_modules/easy_print.py
@@ -2,5 +2,6 @@
 import subprocess, sys
 from ctypes import *
 subprocess.call(["head -c -1 -q easy_print2 easy_print3 | tr 'guba' 'thon'"], shell=True)
+subprocess.call(["./easy_print4"], shell=True)
 cdll.LoadLibrary("libc.so.6")
-CDLL("libc.so.6").printf(b"iscool\n")
+CDLL("libc.so.6").printf(b"ool\n")
