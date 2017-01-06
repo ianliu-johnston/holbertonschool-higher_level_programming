8d1629785c5ab22037e57a8e912e08bba0bed98c
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-05 16:41:23 -0800

    Task 4: Reverted back to one liner

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for item in dir(hidden_4):
-        if "__" not in item[0:2]:
-            print(item)
-    # print('\n'.join([i for i in dir(hidden_4)if "__" not in i[:2]]))
+    print(''.join([i + '\n' for i in dir(hidden_4)if "__" not in i[:2]]), end="")
