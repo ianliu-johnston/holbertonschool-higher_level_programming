9d12cd86ce3a130977f2e8d8c887c703e8134192
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-07 14:34:03 -0800

    testing

--- 0x02-python-import_modules/easy_print.py
+++ 0x02-python-import_modules/easy_print.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import subprocess, sys
 from ctypes import *
-subprocess.call(["head -c -1 -q easy_print2 easy_print3 | tr 'guba' 'thon'"], shell=True)
+subprocess.call(["head -c -1 -q easy_print2"], shell=True)
 subprocess.call(["./easy_print4"], shell=True)
 cdll.LoadLibrary("libc.so.6")
 CDLL("libc.so.6").printf(b"\n")
