41d252c0cf6456cf89829e0f4e8eef53511369b4
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-10 15:53:20 -0800

    Task 2: Updated conditional

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < len(my_list): my_list[idx] = element
+    if 0 <= idx < len(my_list):
+        my_list[idx] = element
     return(my_list)
