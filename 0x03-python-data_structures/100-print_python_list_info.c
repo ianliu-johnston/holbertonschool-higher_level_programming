04bde492a5c3fa3b4b44b39cbd6a1eacd66b6864
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-11 02:08:21 -0800

    Task 100: Fixed for Betty

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -5,16 +5,17 @@
   */
 void print_python_list_info(PyObject *p)
 {
-    Py_ssize_t i, n;
-    PyObject *item;
+	Py_ssize_t i, n;
+	PyObject *item;
 
-    n = PyList_Size(p);
+	n = PyList_Size(p);
 
 	printf("[*] Size of the Python List = %d\n", (int)Py_REFCNT(p));
 	printf("[*] Size of the Python List = %d\n", (int)n);
 	printf("[*] Allocated = %d\n", 0);
-    for (i = 0; i < n; i++) {
-        item = PyList_GetItem(p, i);
+	for (i = 0; i < n; i++)
+	{
+		item = PyList_GetItem(p, i);
 		printf("Element %d: %s\n", (int) i, (char *) Py_TYPE(item)->tp_name);
 	}
 }
