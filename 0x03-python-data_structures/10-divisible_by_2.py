ecd06f70900772db37ced0c5bb802b352ec05452
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-10 20:17:13 -0800

    Task 10: Added enumerate() method on my_list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     boolist = my_list[:]
-    for i in my_list:
+    for count, i in enumerate(my_list):
         if i % 2 == 0:
-            boolist[i] = True
+            boolist[count] = True
         else:
-            boolist[i] = False
+            boolist[count] = False
     return(boolist)
+my_list = [0, 1, 2, 3, 4, 5, 6]
+list_result = divisible_by_2(my_list)
+
+i = 0
+while i < len(list_result):
+    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
+    i += 1
