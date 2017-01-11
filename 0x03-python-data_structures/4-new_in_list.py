4e6737472cc0284bf27182e9f2444fe084e24186
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-10 17:10:33 -0800

    Task 4: Took out debug

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     tmp_list = my_list[:]
-    tmp_list[idx] = element if idx < len(my_list) else "nope"
-    return(tmp_list)
+    if 0 <= idx < len(my_list):
+        tmp_list[idx] = element
+        return(tmp_list)
+    return(my_list)
