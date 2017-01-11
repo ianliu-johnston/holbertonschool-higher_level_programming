c70899eee7967e6fa1ea712155999e6c07f44a6e
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-10 20:10:08 -0800

    Task 8: Deleted tests

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,9 +2,5 @@
 def multiple_returns(sentence):
     length = len(sentence)
     first_char = sentence[0] if length > 0 else "None"
-    tup = length, sentence[0]
+    tup = length, first_char
     return(tup)
-
-sentence = "At Holberton school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
