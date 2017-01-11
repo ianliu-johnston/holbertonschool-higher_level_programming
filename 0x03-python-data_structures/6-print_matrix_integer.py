356b68036ac8f40d293b50eccefb0eb02ec63596
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-10 21:06:24 -0800

    Task 6: Fixed problem with list comprehension

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) == 0:
-        print("")
     for i in matrix:
-        for j in i:
-            print('{:d}'.format(j), end=" ")
-        print("")
+        print(' '.join('{:d}'.format(j)for j in i))
