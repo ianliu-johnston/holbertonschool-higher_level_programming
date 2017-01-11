dd186bb7008919ccd0d5f3abfdd343efde647550
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-10 20:33:05 -0800

    Task 9: Added max for a list of all negative numbers

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,6 +4,6 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return ("None")
     for i in my_list:
-        if i > x:
+        if i > x or i == my_list[0]:
             x = i
-    return ('{:d}'.format(x))
+    return (x)
