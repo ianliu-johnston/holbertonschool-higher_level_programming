c941a4c3a67a5b30c80f3f34ef360707039502cd
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-11 02:06:50 -0800

    Task 100: Saving Progress

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,1 +1,20 @@
-#!/bin/bash
+#include "Python.h"
+/**
+  * print_python_list_info - Prints information about python objects
+  * @p: PyObject pointer to print info about
+  */
+void print_python_list_info(PyObject *p)
+{
+    Py_ssize_t i, n;
+    PyObject *item;
+
+    n = PyList_Size(p);
+
+	printf("[*] Size of the Python List = %d\n", (int)Py_REFCNT(p));
+	printf("[*] Size of the Python List = %d\n", (int)n);
+	printf("[*] Allocated = %d\n", 0);
+    for (i = 0; i < n; i++) {
+        item = PyList_GetItem(p, i);
+		printf("Element %d: %s\n", (int) i, (char *) Py_TYPE(item)->tp_name);
+	}
+}
