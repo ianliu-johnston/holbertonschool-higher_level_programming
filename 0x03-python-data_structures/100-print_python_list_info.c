76891f4b9b5e3a1b1b5bce21013986dd232f87c1
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-11 02:41:34 -0800

    Added a compile command for future reference

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -2,6 +2,10 @@
 /**
   * print_python_list_info - Prints information about python objects
   * @p: PyObject pointer to print info about
+  * Compile with:
+  * gcc -Wall -Werror -Wextra -pedantic -std=c99 -shared
+ (* -Wl,-soname,PyList -o libPyList.so -fPIC -I/usr/include/python3.4
+ (* 100-print_python_list_info.c
   */
 void print_python_list_info(PyObject *p)
 {
