5f36ca0fa3c558093444dfcc2f227d54e3e72973
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-10 20:15:41 -0800

    Task 10: Done

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    boolist = my_list[:]
+    for i in my_list:
+        if i % 2 == 0:
+            boolist[i] = True
+        else:
+            boolist[i] = False
+    return(boolist)
