3744ff6d07afaa737c1042898c799bfdf5cb1efb
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-10 21:01:22 -0800

    Task 6: Fixed matrix empty check

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if len(matrix) == 0:
+        print("")
     for i in matrix:
         for j in i:
             print('{:d}'.format(j), end=" ")
