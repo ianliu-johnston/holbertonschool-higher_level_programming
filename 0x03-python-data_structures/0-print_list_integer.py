c03d36d124a85764f32d28135aeba6ba02c26234
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-10 16:58:25 -0800

    Switched back to loop

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    print('\n'.join('{:d}'.format(i)for i in my_list))
+    for i in my_list:
+        print('{:d}'.format(i))
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    print('\n'.join('{:d}'.format(i) for i in my_list[::-1]))
+    for i in my_list[::-1]:
+        print('{:d}'.format(i))
