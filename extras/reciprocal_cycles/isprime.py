2af0c1efdd4bab73b01435b758d20290acf2f413
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-10 21:47:56 -0800

    Changed some logic in sketches, fixed isprime to be better, added .gitignore

--- extras/reciprocal_cycles/isprime.py
+++ extras/reciprocal_cycles/isprime.py
@@ -1,5 +1,10 @@
-def isPrime(n):
-    for i  in range(2, int(i**0.5)+1):
+#!/usr/bin/python3
+def isprime_custom(n):
+    if n == 2 or n == 3 or n == 5:
+        return (True)
+    if n % 2 == 0 or n % 3 == 0 or n % 5 == 0 or n == 1:
+        return (False)
+    for i in range(5, int(n**0.5)+1, 2):
         if i % n == 0:
             return (False)
     return (True)
--- extras/reciprocal_cycles/sketches.py
+++ extras/reciprocal_cycles/sketches.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
 import decimal
+from isprime import isprime_custom
 
-def isPrime(n):
-    for i in range(2, int(n**0.5)+1):
-        if i % n == 0:
-            return (False)
-
-#power2 = 4
-#power2_count = 3
-#power5 = 5
-#power5_count = 2
-for j in range(1, 1000, 1):
-    if isPrime(j) is False:
-        print('1/{:d} = {:2f}'.format(j, decimal.Decimal(1/j)))
-#    if i == power2:
-#        power2 = 2**power2_count
-#        power2_count += 1;
-#        print("Power2 ya!")
-#    elif i == power5:
-#        power5 = 5**power5_count
-#        power5_count += 1;
-#        print("Power5! Awesome!")
-#    elif (i % 100) == 0:
-#        print("Woh, divisible by 100!")
+power2 = 4
+power2_count = 3
+power5 = 5
+power5_count = 2
+for i in range(1, 1000):
+    if i == power2:
+        power2 = 2**power2_count
+        power2_count += 1;
+        print("Power2 ya!")
+    elif i == power5:
+        power5 = 5**power5_count
+        power5_count += 1;
+        print("Power5! Awesome!")
+    elif (i % 100) == 0:
+        print("Woh, divisible by 100!")
+    elif isprime_custom(i) is True:
+        print("Ooh, a prime! ", end="")
+        print('1/{:d} = {:2f}'.format(i, decimal.Decimal(1/i)))
+    else:
+        print('1/{:d} = {:2f}'.format(i, decimal.Decimal(1/i)))
