5ce9e31e65fd9e93e32eccecf5fc58c8ad415953
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-10 21:15:24 -0800

    Task 3: Changed to one liner

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) == 0:
-        print("")
-    for i in my_list[::-1]:
-        print('{:d}'.format(i))
+    print('\n'.join(['{:d}'.format(i)for i in my_list[::-1]]))
