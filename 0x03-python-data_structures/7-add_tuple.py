b2a30a3f6c2ef55b7c66f31535f17ed46a65d8d1
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-10 20:54:31 -0800

    Task 7: Shortened by several lines

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 0:
-        tuple_a = 0, 0
-    if len(tuple_a) == 1:
-        tuple_a += (0,)
-    if len(tuple_b) == 0:
-        tuple_b = 0, 0
-    if len(tuple_b) == 1:
-        tuple_b += (0,)
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
     return(tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
