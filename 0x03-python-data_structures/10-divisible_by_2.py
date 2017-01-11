e798ee262227792c9d9739af1036f952fae07d3b
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-10 20:18:44 -0800

    Task 10: Removed tests

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -7,10 +7,3 @@ def divisible_by_2(my_list=[]):
         else:
             boolist[count] = False
     return(boolist)
-my_list = [0, 1, 2, 3, 4, 5, 6]
-list_result = divisible_by_2(my_list)
-
-i = 0
-while i < len(list_result):
-    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
-    i += 1
