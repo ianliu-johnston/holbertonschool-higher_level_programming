85bd5e25090d24bf84721b1aaec5c16915e0b99f
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-10 21:25:12 -0800

    Task 3: Found out about reversed(list)

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    print('\n'.join(['{:d}'.format(i)for i in my_list[::-1]]))
+    if my_list is None:
+        return
+    for i in reversed(my_list):
+        print('{:d}'.format(i))
