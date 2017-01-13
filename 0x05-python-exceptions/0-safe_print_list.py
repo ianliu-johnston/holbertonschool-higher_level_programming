1fb3887a418cae4fa6fef97e67704b73e197af9b
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-13 14:06:16 -0800

    Task 0: Refactored

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     i = 0
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print(my_list[i], end="")
-    except:
-        print("")
-        return (i)
+        except:
+            print("")
+            return (i)
     print("")
     return (i+1)
