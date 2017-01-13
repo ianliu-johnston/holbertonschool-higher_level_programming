95836fdcbca85b3c20e555b52f531f68bb829d52
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-13 14:59:53 -0800

    Task 0: Added IndexError and ValueError

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     for i in range(x):
         try:
-            print('{}'.format(my_list[i]), end="")
+            print('{:d}'.format(my_list[i]), end="")
             continue
-        except:
+        except IndexError, ValueError:
             break
     print("")
     return (i + 1)
