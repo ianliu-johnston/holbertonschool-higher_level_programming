057411b6eeb84ade579f5f8aa6e2ebcd59b74cfd
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-13 14:58:56 -0800

    Task 0: Removed second print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print('{}'.format(my_list[i]), end="")
+            continue
         except:
-            print("")
             break
     print("")
     return (i + 1)
