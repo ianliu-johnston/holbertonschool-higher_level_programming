37dca8f24d72217b1d51074c440c01010cfd905c
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-13 14:54:27 -0800

    Task 0: Removed {:d}

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     for i in range(x):
         try:
-            print('{:d}'.format(my_list[i]), end="")
+            print('{}'.format(my_list[i]), end="")
             continue
         except:
             print("")
