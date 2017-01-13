72590fc5bdd38bc74e20109eeea6d0202a16400a
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-12 23:53:28 -0800

    Task 100: Concatenated none and empty checks

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list == {}:
-        return 0
+    if my_list == {} or my_list is None:
+        return (0)
     res = 0
     res2 = 0
     for x, y in my_list:
