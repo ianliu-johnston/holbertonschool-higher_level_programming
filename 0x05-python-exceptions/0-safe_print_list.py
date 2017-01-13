fa0bb46e019f1269820478f113ce4bd49381a606
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-13 14:16:14 -0800

    Task 0: trying another

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,6 +4,7 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print('{:d}'.format(my_list[i]), end="")
+            continue
         except:
             print("")
             return (i)
