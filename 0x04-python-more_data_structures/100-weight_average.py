7bd45328cbec1525befdaec6e00d19dddb8d580f
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-12 23:51:58 -0800

    Task 100: Added None check

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if my_list is None:
+        return 0
     res = 0
     res2 = 0
-    tmp = []
     for x, y in my_list:
         res += x * y
         res2 += y
