a4abb8d5399cc82015286de388c6aadf31f61bfd
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-12 22:12:25 -0800

    Task 10: Complete

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-def square_matrix(matrix=[])
+def best_score(my_dict):
+    biggest = max(my_dict.values())
+    for key, value in my_dict.items():
+        if value is biggest:
+            return key
