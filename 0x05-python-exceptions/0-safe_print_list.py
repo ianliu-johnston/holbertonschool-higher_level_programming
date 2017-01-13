e507d6f99e238210a1d8e41eaeee609eaebd11b8
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-13 14:51:58 -0800

    Task 0: Update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print('{:d}'.format(my_list[i]), end="")
             continue
-        except(IndexError):
+        except:
             print("")
             return (i)
     print("")
