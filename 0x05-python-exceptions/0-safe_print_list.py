24c83c82999391e9ef5508aef43c4b554e027305
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-13 14:03:58 -0800

    Task 0: Removed debuggers.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,12 +9,3 @@ def safe_print_list(my_list=[], x=0):
         return (i)
     print("")
     return (i+1)
-
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
