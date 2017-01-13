5b75e62de981d4e471a58021580cfef878349ac4
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-12 22:14:20 -0800

    Task 10: Added a check for if dict is none.

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def best_score(my_dict):
+    if my_dict is None:
+        return None
     biggest = max(my_dict.values())
     for key, value in my_dict.items():
         if value is biggest:
