d897cde525bfa92d17f5d52397fb346515a1d27f
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-13 14:57:32 -0800

    Task 0: Replaced return with a break

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,9 +4,8 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print('{}'.format(my_list[i]), end="")
-            continue
         except:
             print("")
-            return (i)
+            break
     print("")
     return (i + 1)
