53be82067971250ba0ebff25723a93907afa39d5
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-13 14:30:34 -0800

    Task 0: Added +1 to range, deleted it from the return

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     i = 0
-    for i in range(x):
+    for i in range(x+1):
         try:
             print('{:d}'.format(my_list[i]), end="")
             continue
@@ -9,4 +9,4 @@ def safe_print_list(my_list=[], x=0):
             print("")
             return (i)
     print("")
-    return (i + 1)
+    return (i)
