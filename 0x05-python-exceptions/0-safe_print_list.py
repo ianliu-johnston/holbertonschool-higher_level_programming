dcef1774b03c2d6a20954714cd16ebe8782847b4
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-13 14:10:59 -0800

    Task 0: Added '{:d}'.format to number

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,18 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     for i in range(x):
         try:
-            print(my_list[i], end="")
+            print('{:d}'.format(my_list[i]), end="")
         except:
             print("")
             return (i)
     print("")
-    return (i+1)
+    return (i + 1)
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
