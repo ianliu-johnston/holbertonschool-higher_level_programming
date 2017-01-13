ee9b662ffa38d8a63998b459856c709f866ceaa5
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-13 14:03:21 -0800

    Task 0: Fixed for exception handling

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,11 +4,11 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print(my_list[i], end="")
-        print("")
-        return (i+1)
     except:
         print("")
         return (i)
+    print("")
+    return (i+1)
 
 my_list = [1, 2, 3, 4, 5]
 
