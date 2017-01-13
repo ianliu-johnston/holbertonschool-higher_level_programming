f5a65a8b257763a7e3434882d7c41dd22a1dc7b2
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-12 23:54:52 -0800

    Task 100: Used the wrong brackets for the empty check

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list == {} or my_list is None:
+    if my_list == [] or my_list is None:
         return (0)
     res = 0
     res2 = 0
