6ec959b899b521cefc0e70f9239a4ec1d5f6e8af
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-12 23:51:09 -0800

    Task 100: Complete

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    res = 0
+    res2 = 0
     tmp = []
     for x, y in my_list:
-        tmp.append(x * y)
-    return (0)
+        res += x * y
+        res2 += y
+    return (res / res2)
