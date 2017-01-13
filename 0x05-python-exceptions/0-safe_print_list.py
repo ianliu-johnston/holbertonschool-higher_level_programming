066cb70816c5bb0036a939dce5fda159dfdd5d7f
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-13 14:47:13 -0800

    Task 0: Changed to IndexError

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     i = 0
-    for i in range(x+1):
+    for i in range(x):
         try:
             print('{:d}'.format(my_list[i]), end="")
             continue
-        except:
+        except(IndexError):
             print("")
             return (i)
     print("")
-    return (i)
+    return (i + 1)
