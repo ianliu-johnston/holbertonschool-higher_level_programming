0f74de306b7fd16d7525b4ea718bb4c53fffb51e
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-12 19:40:31 -0800

    Task 4: COmplete

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
+def only_diff_elements(set_1, set_2):
+    a = set_1.difference(set_2)
+    b = set_2.difference(set_1)
+    return(a.union(b))
