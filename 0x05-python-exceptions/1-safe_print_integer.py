b7c4388fb6b68c7a4d09ed0700b9b8ca1a417776
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-13 14:37:50 -0800

    Task 1: Complete

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    return (value)
-
-value = 89
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = -89
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = "Holberton"
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
+    try:
+        print('{:d}'.format(value))
+    except:
+        return (False)
+    return (True)
