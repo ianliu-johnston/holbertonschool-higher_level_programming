deff2590519dcfe48f33a59c80893b0a058ea0fc
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-12 22:19:07 -0800

    Task 10: Updated with another check

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(my_dict):
-    if my_dict is None:
+    if my_dict is None or my_dict == {}:
         return None
     biggest = max(my_dict.values())
     for key, value in my_dict.items():
