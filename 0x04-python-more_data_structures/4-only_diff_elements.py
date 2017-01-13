784e0bde34a94b04ba94d908f0687e75338e599e
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-12 23:32:48 -0800

    Task 4: Turned it into a one liner

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    a = set_1.difference(set_2)
-    b = set_2.difference(set_1)
-    return(a.union(b))
+    return(set_1.symmetric_differences(set_2))
