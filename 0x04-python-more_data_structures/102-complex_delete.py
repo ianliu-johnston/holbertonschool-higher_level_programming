ea909cc0361c22568d5f5aa96eb9f377e4b01d47
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-12 23:41:41 -0800

    Task 102: added space after ,

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(my_dict, value):
     targets = []
-    for key,key_value in my_dict.items():
+    for key, key_value in my_dict.items():
         if key_value is value:
             targets.append(key)
     for x in targets:
