f749431c86cb4a756adf8e7833c5360f8fefe207
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-14 00:01:04 -0800

    Task 3 Completes

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    return (value)
-
-a = 12
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
-
-a = 12
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
+    result = 0
+    try:
+        result = a / b
+    except TypeError:
+        result = None
+    finally:
+        print('Inside result: {:0.1f}'.format(result))
+        return (result)
