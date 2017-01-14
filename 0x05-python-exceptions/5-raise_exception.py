119dc48f8648ba5d6966fb8cd308eee160937c7d
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-13 23:31:26 -0800

    Task 5: Complete

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def raise_exception():
-    return (value)
-
-
-try:
-    raise_exception()
-except TypeError as te:
-    print("Exception raised")
+    print('{:d}'.format([1,3,4,5]))
