d7182f67ddf80ed783d818537c28f3928e185c31
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-13 23:25:15 -0800

    Task2 Complete

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    return (value)
-
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list_integers(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-
-my_list = [1, 2, 3, "Holberton", 4, 5, [1, 2, 3]]
-nb_print = safe_print_list_integers(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
+    n = 0
+    for i in range(x):
+        try:
+            print('{:d}'.format(my_list[i]), end="")
+            n += 1
+        except (ValueError, TypeError):
+            i += 1
+            continue
+    print("")
+    return (n)
