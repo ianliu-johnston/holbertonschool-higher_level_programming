7394413fb678cb25c7c03c9474ff301dd170a863
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-13 17:16:51 -0800

    Task 0: wtf...

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if my_list == {} or my_list is None:
-        return (0)
     n = 0
     for i in range(x):
         try:
             print('{}'.format(my_list[i]), end="")
             n += 1
-        except IndexError, ValueError:
+        except IndexError:
             break
     print("")
     return (n)
