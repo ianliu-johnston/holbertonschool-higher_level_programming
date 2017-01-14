c6d709381e653606136a49ce32e4c38e9e34a2a2
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-13 17:12:39 -0800

    Task 0: Trying something new...

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,9 +5,9 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     for i in range(x):
         try:
-            print('{:d}'.format(my_list[i]), end="")
-            continue
-        except IndexError, TypeError:
-            pass
+            print('{}'.format(my_list[i]), end="")
+        except:
+            print("")
+            return (i)
     print("")
     return (i + 1)
