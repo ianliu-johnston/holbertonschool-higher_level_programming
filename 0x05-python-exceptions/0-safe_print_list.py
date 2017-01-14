037ee75266d38ea1a0db7aee214b615428dca93e
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-13 17:10:59 -0800

    Tas 0: Tried pass

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,8 +7,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print('{:d}'.format(my_list[i]), end="")
             continue
-        except:
-            print("")
-            return (i)
+        except IndexError, TypeError:
+            pass
     print("")
     return (i + 1)
