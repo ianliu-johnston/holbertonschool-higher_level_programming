2eec267f986d5b52d168edfd2046cd790e5452c8
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-13 23:56:19 -0800

    Task 4: Complete

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    return (value)
-
-my_l_1 = [10, 8, 4]
-my_l_2 = [2, 4, 4]
-result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-print(result)
-
-print("--")
-
-my_l_1 = [10, 8, 4, 4]
-my_l_2 = [2, 0, "H", 2, 7]
-result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-print(result)
+    result = []
+    for i in range(list_length):
+        try:
+            result.append(my_list_1[i] / my_list_2[i])
+            continue
+        except ZeroDivisionError:
+            print("division by 0")
+            result.append(0)
+        except TypeError:
+            print("wrong type")
+            result.append(0)
+        except IndexError:
+            print("out of range")
+            result.append(0)
+        finally:
+            pass
+    return(result)
