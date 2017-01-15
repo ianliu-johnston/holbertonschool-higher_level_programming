4930b0cb05fb858578ee4fbc7d94bf2fb1039dcd
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-15 14:43:45 -0800

    Task 100: Fixed for pep8

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 from sys import stderr
 
+
 def safe_print_integer_err(value):
     try:
         print('{:d}'.format(value))
-        return (True)
     except ValueError as te:
         stderr.write('Exception: {}\n'.format(te))
         return (False)
+    return (True)
