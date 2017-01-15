aac9f53d9de18be6a82ed07b9acee5ae4318a8eb
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-15 14:50:21 -0800

    Task 100: Added typeerror

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ from sys import stderr
 def safe_print_integer_err(value):
     try:
         print('{:d}'.format(value))
-    except ValueError as te:
+    except (ValueError, TypeError) as te:
         stderr.write('Exception: {}\n'.format(te))
         return (False)
     return (True)
