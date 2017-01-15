7b6f6bdd7e00b3792620daaf73720eae174ddf80
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-15 14:04:57 -0800

    Task 100: Complete

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-def safe_print_integer_err(value)
+from sys import stderr
+
+def safe_print_integer_err(value):
+    try:
+        print('{:d}'.format(value))
+        return (True)
+    except ValueError as te:
+        stderr.write('{}\n'.format(te))
+        return (False)
+    return (value)
