229e3b30f53dc6b7ee95b33ecec8bc5b3da6b511
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-15 16:05:05 -0800

    Task 101: Complete

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-def safe_function(fct, *args)
+from sys import stderr
+
+def safe_function(fct, *args):
+    try:
+        res = fct(*args)
+        return (res)
+    except Exception as err:
+        stderr.write('Exception: {}\n'.format(err))
+        return (None)
