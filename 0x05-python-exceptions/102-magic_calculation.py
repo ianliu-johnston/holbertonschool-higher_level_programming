ae1c611eef17d029649383831385259edd0b9ca0
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-15 18:29:59 -0800

    Task 102: Complete

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
-void print_python_list(PyObject *p)
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += a ** b / i
+        except:
+            result = a + b
+            break
+    return (result)
