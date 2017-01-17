8491b3d5ee1f2844326c153cf048c89d9ba9cc46
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-17 13:53:31 -0800

    Updating to accomodate for lines with more than one file. Also added a check if there are complete the source code assignments

--- setup.sh
+++ setup.sh
@@ -15,14 +15,21 @@ cd $DIR
 echo -e "a.out\n*.swp\n~*\n_betty-s\n_betty-d\n__pycache__\n" >> .gitignore
 #Create the files
 echo "Creating the files and directory structure"
-touch $(grep File: $INPUT | cut -d \> -f3 | cut -d \< -f1)
+echo "Creating tests directory"
+mkdir tests
+touch $(grep File: $INPUT | cut -d \> -f3 | cut -d \< -f1 | tr -d ',')
 echo '#!/usr/bin/python3' > py_template
 echo "REPLACE" >> py_template
 echo '#!/bin/bash' > sh_template
 find . -type f -empty -exec cp sh_template '{}' \; -exec chmod u+x '{}' \;
 find . -type f -name "*.py" -exec cp py_template '{}' \;
 rm *template
-wget -N -q $(grep -e "source code</a>" -e "here</a>" $INPUT | sed 's/<a href=\"/\n/g' | grep "http" | cut -d \" -f1 | sed 's/github/raw.githubusercontent/;s|blob/||')
+# Check if any complete-the-code downloads are present.
+COMPLETE_THE_CODE=$(grep -e "source code</a>" -e "here</a>" $INPUT | sed 's/<a href=\"/\n/g' | grep "http" | cut -d \" -f1 | sed 's/github/raw.githubusercontent/;s|blob/||')
+if [[ $COMPLETE_THE_CODE  ]]; then
+	echo Getting assignment: $COMPLETE_THE_CODE
+	wget -N -q $COMPLETE_THE_CODE
+fi
 find . -type f -name "*_py" -exec rename -f 's/_py/\.py/' '{}' \;
 find . -type f -name "*.py" -exec chmod u+x '{}' \;
 #Prototypes
@@ -31,9 +38,7 @@ I=0
 while read c; do
 	I=$(($I+1))
 	PROTO=$(echo $c | rev | cut -c 2- | rev)
-	NAME=$(echo $c | cut -d '(' -f 1 | rev | cut -d ' ' -f1 | rev)
 	sed -i "s/REPLACE/$PROTO/g;s/&quot;/\"/g" $(ls -1 | grep "[0-9]-" | sort -h | grep -n "" | grep "$I:" | cut -d : -f2)
-	sed -i "s/main - /$NAME - /g" $(ls -1 | grep "[0-9]-" | sort -h | grep -n "" | grep "$I:" | cut -d : -f2)
 done<prototypes
 rm prototypes
 #README.md
@@ -52,5 +57,4 @@ echo "" >> README.md
 echo "## Description of Files" >> README.md
 ls -1 | grep "[0-9]-" | sort -h | sed 's/^/<h6>/g;s/$/<\/h6>\n/g' >> README.md
 find . -depth -type f -empty -exec rm '{}' \;
-rm ../$INPUT
 echo "Done."
