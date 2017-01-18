a15668efd123630020ac30384d1f0bb4635e3f00
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-17 17:58:59 -0800

    Adding templating functionality for test suites

--- setup.sh
+++ setup.sh
@@ -18,13 +18,33 @@ echo "Creating the files and directory structure"
 echo "Creating tests directory"
 mkdir tests
 touch $(grep File: $INPUT | cut -d \> -f3 | cut -d \< -f1 | tr -d ',')
+#Setup templates for the files, depending on their type.
 echo '#!/usr/bin/python3' > py_template
 echo "REPLACE" >> py_template
 echo "    return (0)" >> py_template
 echo "print(REPLACE)" >> py_template
 echo '#!/bin/bash' > sh_template
+echo "The TEST Module:" > test_template
+echo "    >>> add_integer = __import__(\"TEST\").TEST_FXN" >> test_template
+echo "Make Sure Documentation Exists:" >> test_template
+echo "" >> test_template
+echo "    >>> module_documentation = __import__(\"TEST\").__doc__" >> test_template
+echo "    >>> print(len(module_documentation) > 0)" >> test_template
+echo "    True" >> test_template
+echo "" >> test_template
+echo "    >>> function_documentation = __import__(\"TEST\").TEST_FXN.__doc__" >> test_template
+echo "    >>> print(len(function_documentation) > 0)" >> test_template
+echo "    True" >> test_template
+echo -e "\nCorrect Usage:\n" >> test_template
+echo "    >>> print(TEST_FXN(1, 2))" >> test_template
+echo "    3" >> test_template
+echo -e "\nOn Errors:\n" >> test_template
+echo "    >>> print(TEST_FXN(4, \"School\"))" >> test_template
+echo "    Traceback (most recent call last):" >> test_template
+echo "    TypeError: b must be an integer" >> test_template
 find . -type f -name "*.sh" -exec cp sh_template '{}' \; -exec chmod u+x '{}' \;
 find . -type f -name "*.py" -exec cp py_template '{}' \; -exec chmod u+x '{}' \;
+find ./tests/ -type f -name "*.txt" -exec cp test_template '{}' \;
 rm *template
 # Check if any complete-the-code downloads are present.
 COMPLETE_THE_CODE=$(grep -e "source code</a>" -e "here</a>" $INPUT | sed 's/<a href=\"/\n/g' | grep "http" | cut -d \" -f1 | sed 's/github/raw.githubusercontent/;s|blob/||')
@@ -40,8 +60,15 @@ while read c; do
 	I=$(($I+1))
 	PROTO=$(echo $c | rev | cut -c 2- | rev)
 	NAME=$(echo $PROTO | sed 's/def //')
-	sed -i "s/REPLACE/$PROTO:/g;s/&quot;/\"/g" $(ls -1 | grep "[0-9]-" | sort -h | grep -n "" | grep "$I:" | cut -d : -f2)
-	sed -i "\$s/$PROTO/$NAME/;\$s/://g" $(ls -1 | grep "[0-9]-" | sort -h | grep -n "" | grep "$I:" | cut -d : -f2)
+	FXN_FILENAME=$(ls -1 | grep "[0-9]-" | sort -h | grep -n "" | grep "$I:" | cut -d : -f2)
+	TEST_FILENAME=$(echo $FXN_FILENAME | sed 's/.py/.txt/')
+	TEST=$(echo $FXN_FILENAME | cut -d '.' -f1)
+	TEST_FXN=$(echo $NAME | cut -d '(' -f1)
+	sed -i "s/REPLACE/$PROTO:/g;s/&quot;/\"/g" $FXN_FILENAME
+	sed -i "\$s/$PROTO/$NAME/;\$s/://g" $FXN_FILENAME
+	## For test Replacement:
+	sed -i "s/TEST_FXN/$TEST_FXN/g" tests/$TEST_FILENAME
+	sed -i "s/TEST/$TEST/g;" tests/$TEST_FILENAME
 done<prototypes
 rm prototypes
 #README.md
