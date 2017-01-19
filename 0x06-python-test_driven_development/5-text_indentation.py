3843ce133456c3091248c2749dd147fdc64e4509
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-18 16:30:34 -0800

    Task 5: Complete

--- 0x06-python-test_driven_development/5-text_indentation.py
+++ 0x06-python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+"""
+Text_indentation Module
+adds two new lines after "?.:"
+doesn't print any spaces at the beginning or end of the sentences.
+"""
 def text_indentation(text):
-    return (0)
-
-print(text_indentation(text))
+    """
+    text_indentation function:
+    checks to see if input is valid
+    adds two new lines after any instances of `?` or `.` or `:`
+    then prints the result without any new lines at the beginning
+    """
+    if text is None or not isinstance(text, str) or len(text) < 0:
+        raise TypeError("text must be a string")
+    new_txt = "".join([c if c not in "?.:" else c + "\n\n" for c in text])
+    for line in new_txt.split("\n"):
+        print(line.strip())
