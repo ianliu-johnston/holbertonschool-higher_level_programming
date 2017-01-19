5ca624a8419ff62ecfed400b180d3b75717407f6
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-18 18:46:39 -0800

    Task 3: Replaced elif with if

--- 0x06-python-test_driven_development/3-say_my_name.py
+++ 0x06-python-test_driven_development/3-say_my_name.py
@@ -13,7 +13,7 @@ def say_my_name(first_name, last_name=""):
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    elif not isinstance(last_name, str):
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     try:
         return ("My name is {} {}".format(first_name, last_name))
