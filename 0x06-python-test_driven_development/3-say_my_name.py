f8b0c9b9a5a91eb1577052bde7627ad19afd6621
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-18 18:54:55 -0800

    Task 3: Changed return to print

--- 0x06-python-test_driven_development/3-say_my_name.py
+++ 0x06-python-test_driven_development/3-say_my_name.py
@@ -16,6 +16,6 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     try:
-        return ("My name is {} {}".format(first_name, last_name))
+        print ("My name is {} {}".format(first_name, last_name))
     except:
         raise
