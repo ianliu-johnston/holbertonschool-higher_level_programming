295ccefd203707d0e9f038f7a392bfe86cb5b283
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-18 19:09:13 -0800

    Task 3: Idk

--- 0x06-python-test_driven_development/3-say_my_name.py
+++ 0x06-python-test_driven_development/3-say_my_name.py
@@ -15,4 +15,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    print ("My name is {} {}".format(first_name, last_name))
+    print("My name is {} {}".format(first_name, last_name))
