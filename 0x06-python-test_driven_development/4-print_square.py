c92cc40791f0f7ba35f85fb0352ced47d8bc9a1e
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-18 17:59:41 -0800

    Task 4: Removed printing of a new line

--- 0x06-python-test_driven_development/4-print_square.py
+++ 0x06-python-test_driven_development/4-print_square.py
@@ -15,6 +15,8 @@ def print_square(size):
         raise TypeError("size must be an integer")
     elif size < 0:
         raise ValueError("size must be >= 0")
+    elif size == 0:
+        return
     else:
         print("\n".join([
             "".join(["#" for x in range(size)])
