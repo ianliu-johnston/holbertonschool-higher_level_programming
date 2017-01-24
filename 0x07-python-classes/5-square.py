08da806c7e59dfcc75cdcd9469811f26a6adc412
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-24 00:08:13 -0800

    Task 5: Complete

--- 0x07-python-classes/5-square.py
+++ 0x07-python-classes/5-square.py
@@ -1,4 +1,37 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+"""
+Super simple Square Module.
+
+"""
+
+
+class Square:
+    """
+    Only does one thing: Gives you its area.
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        print("\n".join([''.join(["#" for y in range(self.__size)])
+                         for x in range(self.__size)]))
