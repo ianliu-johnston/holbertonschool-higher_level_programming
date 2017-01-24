659995cee5e92637eceafd8338486e3ff095fd0e
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-24 03:38:00 -0800

    Saving my main file

---
+++ 0x07-python-classes/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+Square = __import__('5-square').Square
+
+my_square = Square(89)
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+my_square.size = 3
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+
+my_square.my_print()
