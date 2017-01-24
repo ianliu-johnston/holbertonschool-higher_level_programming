1b65a7d09e3582f37678f3676299302e182ffca3
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-24 00:22:16 -0800

    Task 1: Complete

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        if not isinstance((height, width), int):
+            wrong = "height" if not isinstance(height, int) else "width"
+            raise TypeError(wrong + " must be an integer")
+        if height < 0 or width < 0:
+            wrong = "height" if height < 0 else "width"
+            raise ValueError(wrong + " must be >= 0")
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return(self.__width)
+
+    @property
+    def height(self):
+        return(self.__height)
