98f8f79242f2211fe21ebb3ba7bcc30665dfc9f2
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-24 04:03:22 -0800

    Task 101: Saving progress for later

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+from sys import argv
+
+ac = len(argv)
+if ac != 2:
+    print("What the fuck do you think you're doing?? Usage: nqueens N")
+    exit(1)
+elif argv[1].isdigit() is False:
+    print("You fucked up. N must be a number")
+    exit(1)
+elif int(argv[1]) < 4:
+    print("Seriously? No. I don't fucking think so. N must be at least 4")
+    exit(1)
+
+size = int(argv[1])
+chess_board = [["$" for y in range(size)] for x in range(size)]
+print ( chess_board )
+print("{:d}".format(int(argv[1])))
