098011f7ff6eff79a858e6997bc136116ee13b76
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-24 21:48:01 -0800

    Task 1: Reconfigured

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,11 +13,6 @@ class Rectangle:
     @property
     def width(self):
         return(self.__width)
-
-    @property
-    def height(self):
-        return(self.__height)
-
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -26,6 +21,9 @@ class Rectangle:
             raise TypeError("width must be an integer")
         self.__width = value
 
+    @property
+    def height(self):
+        return(self.__height)
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
