c56159e63e99c42e44af0c1fad644f60f6a86d8d
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-24 21:58:56 -0800

    Task 2: Changed properties and setter methods to reflect 1

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -14,24 +14,24 @@ class Rectangle:
     def width(self):
         return(self.__width)
 
-    @property
-    def height(self):
-        return(self.__height)
-
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise TypeError("width must be an integer")
+            raise ValueError("width must be >= 0")
         self.__width = value
+
+    @property
+    def height(self):
+        return(self.__height)
 
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
-            raise TypeError("height must be an integer")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
