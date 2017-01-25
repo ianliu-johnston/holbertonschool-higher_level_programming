30543b1f4acb8db1c34165f19a1b4cf575a81248
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-01-24 23:35:50 -0800

    Task 4: Fixed to reflect 3

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -11,39 +11,43 @@ class Rectangle:
         self.__height = height
 
     def __str__(self):
-        return ("\n".join(["".join(["#" for y in range(self.__width)])
-                           for x in range(self.__height)]))
+        if self.__width > 0 and self.__height > 0:
+            return ("\n".join(["".join(["#"
+                               for y in range(self.__width)])
+                               for x in range(self.__height)]))
 
     def __repr__(self):
-        return ("Rectangle(" + "{:d}".format(self.__width) + ", " +
-                "{:d}".format(self.__height) + ")")
+        return ("Rectangle({:d}, {:d})".format(self.__width, self.__height))
 
     @property
     def width(self):
         return(self.__width)
-
-    @property
-    def height(self):
-        return(self.__height)
 
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise TypeError("width must be an integer")
+            raise ValueError("width must be >= 0")
         self.__width = value
+
+    @property
+    def height(self):
+        return(self.__height)
 
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
-            raise TypeError("height must be an integer")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
         return(self.__width * self.__height)
 
     def perimeter(self):
-        return(self.__width * 2 + self.__height * 2)
+        if self.__width > 0 and self.__height > 0:
+            return(self.__width * 2 + self.__height * 2)
+        else:
+            return (0)
