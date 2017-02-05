27e29d11ebc251d72da32beec67cc57c88de775e
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-04 23:14:47 -0800

    Task 0: Complete

---
+++ 0x0B-python-input_output/0-main.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+read_file = __import__('0-read_file').read_file
+
+read_file("theDreamQuestOfTheUnknownKadath.txt")
--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-def read_file(filename="");
-    with open(filename, encoding = "utf-8") as f:
-        print(f.readlines())
+def read_file(filename=""):
+    with open(filename, encoding="utf-8") as f:
+        lineNum = 1
+        while(True):
+            line = f.readline()
+            if not line:
+                break
+            print(line, end="")
+            lineNum += 1
