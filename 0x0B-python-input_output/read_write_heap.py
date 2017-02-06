fc3b7793b1548128f41089f7c612a2f973f11ff3
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-05 22:37:09 -0800

    Saving for later

--- 0x0B-python-input_output/read_write_heap.py
+++ 0x0B-python-input_output/read_write_heap.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """
-./read_write_heap.py `ps -aux | grep dummy.py | grep -v "grep" | cut -d ' ' -f 3` <string> <replace>
+./read_write_heap.py `ps -aux | grep dummy.py | grep -v "grep" | cut -d ' ' -f 3` string replace
 
 
 """
-
 import os
 from sys import argv, exit
 def check_args():
-    if len(argv) != 4:
+    if len(argv) != 2:
         print("{} pid search_string replace_string".format(argv[0]))
         exit(1)
     pid = argv[1]
@@ -19,15 +18,19 @@ def check_args():
         exit(1)
     return(pid)
 
-def display_info(pid=1):
-    with open("/proc/" + str(pid) + "/status", 'r') as f:
+def get_heap_memory_addr(pid=1):
+    with open("/proc/" + str(pid) + "/maps", 'r') as f:
         while True:
             line = f.readline()
+            if '[heap]' in line:
+                f.close()
+                return (line[:8], line[9:16])
             if not line:
                 break
-            print(line, end="")
         f.close()
+    return "Error, no address found"
 
 if __name__ == "__main__":
     pid = check_args()
-    display_info(pid)
+    memaddr = get_heap_memory_addr(pid)
+    print(memaddr)
