4ac1ed8fed1e01c99fb0cb9b2f29f34ac106b945
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-24 00:16:06 -0800

    Task 100: Not working, but saving progress

--- 0x0C-SQL_introduction/100-move_to_utf8.sql
+++ 0x0C-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,5 @@
-#!/usr/bin/env bash
-#DESCRIPTION OF FUNCTIONALITY
+-- Converts parts of the database to UTF-8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- doesn't work. ALTER TABLE first_table MODIFY name VARBINARY(255);
