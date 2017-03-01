915f83adf965f9ce5ea61d5d0b20ead98b4fc392
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-28 22:43:36 -0800

    Task 7: Removed USE line and expanded the commment

--- 0x0D-SQL_more_queries/7-cities.sql
+++ 0x0D-SQL_more_queries/7-cities.sql
@@ -1,6 +1,5 @@
--- db
+-- Creates the database and a table that references another table as a parent 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
 	id INT UNIQUE AUTO_INCREMENT PRIMARY KEY, 
 	state_id INT NOT NULL, 
