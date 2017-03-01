15b907af65fd154bf9080d3efc37b4a59cd266e4
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-28 18:22:46 -0800

    Task 2: Complete

---
+++ 0x0D-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- Create a database and a user, then assign the user select rights to the database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
