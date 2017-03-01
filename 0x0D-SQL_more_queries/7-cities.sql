2cecb4170f664651068c3e59c7d2345b1329c63a
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-28 22:51:40 -0800

    Task 7: Added USE database

--- 0x0D-SQL_more_queries/7-cities.sql
+++ 0x0D-SQL_more_queries/7-cities.sql
@@ -1,5 +1,6 @@
 -- Creates the database and a table that references another table as a parent 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
 	id INT AUTO_INCREMENT PRIMARY KEY, 
 	state_id INT NOT NULL, 
