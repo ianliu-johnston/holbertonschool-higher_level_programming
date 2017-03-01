33f4fe0d1d8f1282d59c244dd71ca4b2a636a1c8
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-28 23:19:56 -0800

    Task 8: Fixed typo

--- 0x0D-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0D-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- Select cities by state
-SELECT id, name FROM cities WHERE states_id = (SELECT id FROM states WHERE name = 'California' )ORDER BY cities.id ASC;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California' )ORDER BY cities.id ASC;
