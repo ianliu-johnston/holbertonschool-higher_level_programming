d159275ec5c2da5a524f0ee42ddbf3977dc23578
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-01 00:09:50 -0800

    Task 13: Got the right output with GROUP BY

--- 0x0D-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0D-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,4 +3,6 @@ SELECT tv_genres.name AS `genre`, COUNT(*) AS `number_shows`
 FROM tv_genres JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_show_genres.show_id IS NOT NULL
+GROUP BY tv_genres.name
 ORDER BY `number_shows` DESC;
+
