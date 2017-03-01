18a8795ae314e99395d74225734f383899f1d71f
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-28 18:14:27 -0800

    Task 1: Complete

---
+++ 0x0D-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Creates a new MySQL user with a password and grants full permissions
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
