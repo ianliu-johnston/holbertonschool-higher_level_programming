458cb07fd46d08d53604cf47444c19a1cb7cb437
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-01 00:07:16 -0800

    Task 13: Still have a little bit of time

--- 0x0D-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0D-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Select results of the search if not null and the count of each instancel
 SELECT tv_genres.name AS `genre`, COUNT(*) AS `number_shows`
-FROM tv_shows JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
+FROM tv_genres JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_show_genres.show_id IS NOT NULL
 ORDER BY `number_shows` DESC;
