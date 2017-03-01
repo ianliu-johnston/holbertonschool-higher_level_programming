33ebd2a4d1b4be330f51dba326850215815591b9
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-28 23:02:12 -0800

    Task 7: COmplete

--- 0x0D-SQL_more_queries/7-cities.sql
+++ 0x0D-SQL_more_queries/7-cities.sql
@@ -4,6 +4,6 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
 	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, 
 	state_id INT NOT NULL, 
-	FOREIGN ID(state_id) REFERENCES states(id),
+	FOREIGN KEY(state_id) REFERENCES states(id),
 	name VARCHAR(256) NOT NULL
-);
+)
