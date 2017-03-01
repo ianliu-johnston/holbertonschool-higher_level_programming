67c6801080e6c68eca3c3c0b20c6f964cc7241af
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-28 22:04:23 -0800

    Task 10: Complete

--- 0x0D-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0D-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,5 @@
 -- select rows from different tables
-SELECT title, genre_id FROM tv_shows, tv_show_genres ORDER BY title, genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY title, genre_id ASC;
