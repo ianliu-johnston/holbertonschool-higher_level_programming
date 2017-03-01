82bd394bd811070a118058d8909912d9321ab7e7
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-28 23:43:20 -0800

    Task 11: Added comment

--- 0x0D-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0D-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,3 +1,4 @@
+-- displays tv shows using left join, displays null when value is not set
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
