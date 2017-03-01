f59b245b2112877bfe97147efbaecbf22a3385df
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-28 21:35:36 -0800

    task 10: just checking to see if it works

--- 0x0D-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0D-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,3 @@
 -- Adds a .sql file to a database.
-CREATE DATABASE IF NOT EXISTS hbtn_0d_tvshows;
-USE hbtn_0d_tvshows;
 SOURCE hbtn_0d_tvshows.sql
 SELECT title, genre_id from tv_shows, tv_show_genres ORDER BY genre_id ASC;
