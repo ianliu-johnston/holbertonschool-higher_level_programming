c5ef8e0cadbad63de3119c4677e08198ea72b171
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-28 22:41:26 -0800

    Task 7: Complete

---
+++ 0x0D-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- db
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+	id INT UNIQUE AUTO_INCREMENT PRIMARY KEY, 
+	state_id INT NOT NULL, 
+	FOREIGN ID(states_id) REFERENCES hbtn_0d_usa.states(id),
+	name VARCHAR(256)
+);
