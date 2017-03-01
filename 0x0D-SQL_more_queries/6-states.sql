36581a0ff4e177937882d6e5ee4d4daf65c5903e
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-28 22:25:52 -0800

    Task 6: added a comment

--- 0x0D-SQL_more_queries/6-states.sql
+++ 0x0D-SQL_more_queries/6-states.sql
@@ -1,3 +1,4 @@
+-- create a database and a table in that database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256));
