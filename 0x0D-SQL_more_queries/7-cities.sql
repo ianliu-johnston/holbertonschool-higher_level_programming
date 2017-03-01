679f8b0f4aa619cf8b4127db0c34f86ee06858e6
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-28 22:47:42 -0800

    Task 7: removed some unnecessary lines

--- 0x0D-SQL_more_queries/7-cities.sql
+++ 0x0D-SQL_more_queries/7-cities.sql
@@ -1,8 +1,8 @@
 -- Creates the database and a table that references another table as a parent 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
-	id INT UNIQUE AUTO_INCREMENT PRIMARY KEY, 
+	id INT AUTO_INCREMENT PRIMARY KEY, 
 	state_id INT NOT NULL, 
-	FOREIGN ID(state_id) REFERENCES hbtn_0d_usa.states(id),
+	FOREIGN ID(state_id) REFERENCES states(id),
 	name VARCHAR(256) NOT NULL
 );
