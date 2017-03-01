784cdaff914f72cee5789bfa5a65db927a9dd03a
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-28 22:57:49 -0800

    Task 7: added a space

--- 0x0D-SQL_more_queries/7-cities.sql
+++ 0x0D-SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- Creates the database and a table that references another table as a parent 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(
+CREATE TABLE IF NOT EXISTS cities (
 	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, 
 	state_id INT NOT NULL, 
 	FOREIGN ID(state_id) REFERENCES states(id),
