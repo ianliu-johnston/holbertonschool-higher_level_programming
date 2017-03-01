dadb5d9b0793a31063a3659ed0f86d9373bd53d2
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-28 23:47:08 -0800

    Task 12 Complete

---
+++ 0x0D-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- Select results of the search if null
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
