9e16b46c94cc250116faf0a6266ef4cb735567ba
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-28 23:26:22 -0800

    Task 9: typo.

--- 0x0D-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0D-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- lists all cities contained in the DB
 SELECT cities.id, cities.name, states.name
 FROM cities JOIN states 
-ON cities.id = states.states_id
+ON cities.id = states.state_id
 ORDER BY cities.id ASC;
