6f1c21bb8dc0a788a6bb96f29143168c7a6329b4
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-09 00:10:07 -0800

    Adding Model city

--- 0x0E-python-object_relational_mapping/model_city.py
+++ 0x0E-python-object_relational_mapping/model_city.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+from model_state import Base, State
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class City(Base, State):
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String(128))
+    state_id = Column(Integer, ForeignKey(State.id))
