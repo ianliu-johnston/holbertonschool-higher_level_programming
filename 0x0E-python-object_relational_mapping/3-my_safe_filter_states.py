2ba3aa3a41395fc8c2b30182aabfdba805076f4d
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-08 19:27:14 -0800

    Task 3: Complete

--- 0x0E-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0E-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,4 +1,30 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+# Lists all states from a database
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv, exit
+
+    if len(argv) != 5:
+        print("Usage: ./2.py <username> <password> <database> <search>")
+        exit(1)
+
+    usr = argv[1]
+    pwd = argv[2]
+    dbe = argv[3]
+    sea = argv[4].split("'")[0]
+
+    try:
+        database = MySQLdb.Connect(user=usr, passwd=pwd, db=dbe, port=3306)
+    except Exception as err:
+        print(err)
+        exit(1)
+    cursor = database.cursor()
+    cursor.execute("""
+        SELECT * FROM states WHERE states.name = %s
+        ORDER BY states.id ASC
+    """, (sea, ))
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    database.close()
