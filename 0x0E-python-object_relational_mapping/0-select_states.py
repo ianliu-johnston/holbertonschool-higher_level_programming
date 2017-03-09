1fc1ea4ba6534cb2d7a2f8613efaf7552b0dd296
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-08 18:04:02 -0800

    Task 0: Previously, I used sqlalchemy, switched to MySQLdb

--- 0x0E-python-object_relational_mapping/0-select_states.py
+++ 0x0E-python-object_relational_mapping/0-select_states.py
@@ -2,17 +2,17 @@
 # Lists all states from a database
 
 if __name__ == "__main__":
-    import sqlalchemy, MySQLdb
+    import MySQLdb
     from sys import argv, exit
 
     if len(argv) != 4:
         print("Usage: {:s} <username> <password> <database name>".format(argv[0]))
         exit(1)
-    engine = sqlalchemy.create_engine('mysql://' + argv[1] + ':' \
-            + argv[2] + "@localhost/" + argv[3])
-    connection = engine.connect()
-    states = connection.execute("SELECT * FROM states")
+    database = MySQLdb.Connect(user=argv[1], passwd=argv[2], \
+            db=argv[3], port=3306)
+    cursor = database.cursor()
+    cursor.execute("SELECT * FROM states")
+    states = cursor.fetchall()
     for row in states:
         print(row)
-    connection.close()
 
