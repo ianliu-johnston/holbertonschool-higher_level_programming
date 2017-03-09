107302f51b1e579e80527df82aa0824f4257f76c
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-08 21:12:47 -0800

    Task 6: Complete

--- 0x0E-python-object_relational_mapping/model_state.py
+++ 0x0E-python-object_relational_mapping/model_state.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String(128))
