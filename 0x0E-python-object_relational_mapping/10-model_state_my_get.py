91f7a0462d122b9f2a9a78236dc156ba605a7077
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-08 23:32:43 -0800

    Task 10: Complete

--- 0x0E-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0E-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,4 +1,32 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+# Lists all states from a database
+
+if __name__ == "__main__":
+    import sqlalchemy
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+    from sys import argv, exit
+
+    if len(argv) != 5:
+        print("Usage: ./10.py <usrname> <passwd> <database> <search>")
+        exit(1)
+
+    usr, pwd, dbe = argv[1], argv[2], argv[3]
+    sch = argv[4].split("'")[0]
+
+    eng = "mysql://" + usr + ":" + pwd + "@localhost:3306/" + dbe
+    try:
+        engine = sqlalchemy.create_engine(eng)
+    except Exception as err:
+        print(err)
+        exit(1)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(State).filter_by(name=sch)
+    if query.first() is None:
+        print("Not Found")
+    else:
+        for rows in session.query(State).filter_by(name=sch):
+            print("{:d}".format(rows.id))
+    session.close()
