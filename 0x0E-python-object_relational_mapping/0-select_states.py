c30a405d6bdced462e97cedd923f11c8acf9063b
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-08 18:14:18 -0800

    Task 0: Fixed for pep8

--- 0x0E-python-object_relational_mapping/0-select_states.py
+++ 0x0E-python-object_relational_mapping/0-select_states.py
@@ -6,13 +6,16 @@ if __name__ == "__main__":
     from sys import argv, exit
 
     if len(argv) != 4:
-        print("Usage: {:s} <username> <password> <database name>".format(argv[0]))
+        print("Usage: {:s} <username> <password> <database>".format(argv[0]))
         exit(1)
-    database = MySQLdb.Connect(user=argv[1], passwd=argv[2], \
-            db=argv[3], port=3306)
+
+    usr = argv[1]
+    pwd = argv[2]
+    dbe = argv[3]
+
+    database = MySQLdb.Connect(user=usr, passwd=pwd, db=dbe, port=3306)
     cursor = database.cursor()
     cursor.execute("SELECT * FROM states")
     states = cursor.fetchall()
     for row in states:
         print(row)
-
