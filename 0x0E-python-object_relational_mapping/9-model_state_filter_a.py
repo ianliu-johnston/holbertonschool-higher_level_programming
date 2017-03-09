672e1fc1c9cf05f773ab020f63116e36a2b29c45
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-08 22:51:01 -0800

    Task 9: Complete

--- 0x0E-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0E-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+# Lists all states from a database
+
+if __name__ == "__main__":
+    import sqlalchemy
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+    from sys import argv, exit
+
+    if len(argv) != 4:
+        print("Usage: ./7.py <username> <password> <database>")
+        exit(1)
+
+    usr, pwd, dbe = argv[1], argv[2], argv[3]
+
+    eng = "mysql://" + usr + ":" + pwd + "@localhost:3306/" + dbe
+    try:
+        engine = sqlalchemy.create_engine(eng)
+    except Exception as err:
+        print(err)
+        exit(1)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for rows in session.query(State).filter(State.name.like('%a%')):
+        print ("{:d}: {:s}".format(rows.id, rows.name))
+    session.close()
