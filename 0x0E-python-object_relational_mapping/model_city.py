1a711cf47ba0164e53353f479117b5df7a988c0b
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-09 00:13:27 -0800

    Forgot to include ForeignKey in import

--- 0x0E-python-object_relational_mapping/model_city.py
+++ 0x0E-python-object_relational_mapping/model_city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from model_state import Base, State
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
