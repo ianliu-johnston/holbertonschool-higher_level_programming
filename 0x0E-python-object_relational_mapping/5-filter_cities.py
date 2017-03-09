19ae987cd2b6d293f480f4c28c548791c7cdc7f7
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-08 20:10:23 -0800

    Task 5: Complete

--- 0x0E-python-object_relational_mapping/5-filter_cities.py
+++ 0x0E-python-object_relational_mapping/5-filter_cities.py
@@ -1,4 +1,30 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+# Lists all states from a database
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv, exit
+
+    if len(argv) != 5:
+        print("Usage: ./5.py <username> <password> <database> <search>")
+        exit(1)
+
+    usr, pwd, dbe = argv[1], argv[2], argv[3]
+    sch = argv[4].split("'")[0]
+
+    try:
+        database = MySQLdb.Connect(user=usr, passwd=pwd, db=dbe, port=3306)
+    except Exception as err:
+        print(err)
+        exit(1)
+    cursor = database.cursor()
+    cursor.execute("""
+        SELECT cities.name FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+    """, (sch,))
+    print(", ".join([row[0] for row in cursor.fetchall()]))
+    cursor.close()
+    database.close()
+
