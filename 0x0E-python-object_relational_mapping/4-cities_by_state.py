dc8605b7359a83751e8d89088819c34c793cd0eb
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-08 19:37:34 -0800

    Task 4: Complete

--- 0x0E-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0E-python-object_relational_mapping/4-cities_by_state.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+# Lists all states from a database
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv, exit
+
+    if len(argv) != 5:
+        print("Usage: ./4.py <username> <password> <database>")
+        exit(1)
+
+    usr = argv[1]
+    pwd = argv[2]
+    dbe = argv[3]
+
+    try:
+        database = MySQLdb.Connect(user=usr, passwd=pwd, db=dbe, port=3306)
+    except Exception as err:
+        print(err)
+        exit(1)
+    cursor = database.cursor()
+    cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    database.close()
