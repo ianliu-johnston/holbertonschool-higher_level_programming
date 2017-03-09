d4188d94ad6881f22c50ce67ce779bc09b6c8ac7
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-08 21:37:48 -0800

    Task 7: Complete

--- 0x0E-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0E-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,27 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+# Lists all states from a database
+
+if __name__ == "__main__":
+    import sqlalchemy
+    from model_state import Base, State
+    from sys import argv, exit
+
+    if len(argv) != 4:
+        print("Usage: ./5.py <username> <password> <database>")
+        exit(1)
+
+    usr, pwd, dbe = argv[1], argv[2], argv[3]
+
+    eng = "mysql://" + usr + ":" + pwd + "@localhost:3306/" + dbe
+    try:
+        engine = sqlalchemy.create_engine(eng)
+    except Exception as err:
+        print(err)
+        exit(1)
+    connection = engine.connect()
+    states = engine.execute("""
+        SELECT * FROM states
+        ORDER BY states.id ASC
+    """)
+    print("\n".join(["{:d}: {:s}".format(row[0], row[1]) for row in states]))
+    connection.close()
