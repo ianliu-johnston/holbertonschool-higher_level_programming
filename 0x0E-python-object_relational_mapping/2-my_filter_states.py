08b7a74bf5467fa16a69dda841481247f3d2c1b3
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-08 18:53:52 -0800

    Task 2: Complete

--- 0x0E-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0E-python-object_relational_mapping/2-my_filter_states.py
@@ -1,4 +1,31 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+# Lists all states from a database
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv, exit
+
+    if len(argv) != 5:
+        print("Usage: ./2.py <username> <password> <database> <search>")
+        exit(1)
+
+    usr = argv[1]
+    pwd = argv[2]
+    dbe = argv[3]
+    ser = argv[4]
+
+    try:
+        database = MySQLdb.Connect(user=usr, passwd=pwd, db=dbe, port=3306)
+    except Exception as err:
+        print(err)
+        exit(1)
+    cursor = database.cursor()
+    cursor.execute("""
+        SELECT * FROM states WHERE states.name LIKE '%{:s}%'
+        ORDER BY states.id ASC
+    """.format(ser))
+    for row in cursor.fetchall():
+        if row[1][0] == 'N':
+            print(row)
+    cursor.close()
+    database.close()
