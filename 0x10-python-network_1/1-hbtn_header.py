7ddbda11bd03255e6efff677f78770520342ab09
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-11 12:58:59 -0700

    Task 1: Complete

--- 0x10-python-network_1/1-hbtn_header.py
+++ 0x10-python-network_1/1-hbtn_header.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+"""
+la
+"""
+import urllib.request
+from sys import argv
+
+
+if __name__ == "__main__":
+    req = str(argv[1])
+    with urllib.request.urlopen(req) as response:
+        html = response.info()
+    print('{}'.format(html['X-Request-Id']))
