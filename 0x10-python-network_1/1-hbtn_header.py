e57dec85b8214facb4e1bd637182626d00727f37
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-11 13:36:52 -0700

    Task 1: Removed format string

--- 0x10-python-network_1/1-hbtn_header.py
+++ 0x10-python-network_1/1-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Grabs the header "X-Request-Id" from a url passed as an argument
+Grabs the header X-Request-Id from a url passed as an argument
 """
 import urllib.request
 from sys import argv
@@ -9,4 +9,4 @@ from sys import argv
 if __name__ == "__main__":
     req = urllib.request.Request(str(argv[1]))
     with urllib.request.urlopen(req) as response:
-        print('{}'.format(response.getheader('X-Request-Id')))
+        print(response.getheader('X-Request-Id'))
