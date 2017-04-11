34bfea5496f1815e013824e46e5a23e5afecb4fc
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-11 15:55:50 -0700

    Task 10: Complete

--- 0x10-python-network_1/10-my_github.py
+++ 0x10-python-network_1/10-my_github.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+"""
+Basic Authentication with github api
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    req = requests.get("https://api.github.com/user", auth=(argv[1], argv[2]))
+    print(req.json()['id'])
