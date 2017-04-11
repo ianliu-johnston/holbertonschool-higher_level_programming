f87e287551717380d2d1c3019ce4d9de8a03f104
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-11 13:45:09 -0700

    Task 1: Removed method=

--- 0x10-python-network_1/1-hbtn_header.py
+++ 0x10-python-network_1/1-hbtn_header.py
@@ -7,6 +7,6 @@ import sys
 
 
 if __name__ == "__main__":
-    req = urllib.request.Request(sys.argv[1], method="HEAD")
+    req = urllib.request.Request(sys.argv[1])
     with urllib.request.urlopen(req) as response:
         print(response.getheader('X-Request-Id'))
