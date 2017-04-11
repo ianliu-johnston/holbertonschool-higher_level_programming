41442db80c1b441609df7b8a41c6a351b8cf48ab
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-11 13:11:55 -0700

    Task 0: Case sensitive output is fixed

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     req = 'https://intranet.hbtn.io/status'
     with urllib.request.urlopen(req) as response:
         html = response.read()
-    print("""Body Response:
+    print("""Body response:
 \t- type:  {}
 \t- content: {}
 \t- utf8 content: {}""".format(type(html), html, html.decode('utf-8')))
