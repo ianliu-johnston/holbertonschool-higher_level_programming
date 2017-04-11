19b555c2811bf2dd9934733572b1926de9939c6f
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-11 13:10:33 -0700

    Task 0: Changed spaces to tabs in print out

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(req) as response:
         html = response.read()
     print("""Body Response:
-    - type:  {}
-    - content: {}
-    - utf8 content: {}""".format(type(html), html, html.decode('utf-8')))
+\t- type:  {}
+\t- content: {}
+\t- utf8 content: {}""".format(type(html), html, html.decode('utf-8')))
