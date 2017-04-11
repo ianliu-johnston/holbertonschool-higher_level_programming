9744de54d46d835881ec3cabab6b564b9a0df5a0
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-11 13:38:34 -0700

    Task 1: Using Anne's Code

--- 0x10-python-network_1/1-hbtn_header.py
+++ 0x10-python-network_1/1-hbtn_header.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """
-Grabs the header X-Request-Id from a url passed as an argument
+This is module 1-hbtn_header.
+This module takes a url as an argument and displays a specific response header
 """
 import urllib.request
-from sys import argv
+import sys
 
 
 if __name__ == "__main__":
-    req = urllib.request.Request(str(argv[1]))
+    req = urllib.request.Request(sys.argv[1], method="HEAD")
     with urllib.request.urlopen(req) as response:
-        print(response.getheader('X-Request-Id'))
+        print(response.getheader("X-Request-Id"))
