fdd2e39e7853f84c312f4cd4d0d2ad25b25a25ff
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-11 16:06:12 -0700

    Task 10: lka

--- 0x10-python-network_1/10-my_github.py
+++ 0x10-python-network_1/10-my_github.py
@@ -6,6 +6,6 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    requests.post("https://intranet.hbtn.co/auth/sign_in", data={"username": argv[2], "password": argv[1]})
+    requests.post("https://intranet.hbtn.co/auth/sign_in", data={"username": argv[2] + argv[1], "password": argv[1]})
     req = requests.get("https://api.github.com/user", auth=(argv[1], argv[2]))
     print(req.json()['id'])
