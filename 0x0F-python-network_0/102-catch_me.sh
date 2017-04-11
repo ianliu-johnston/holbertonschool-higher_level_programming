f08c1821f9d7edb633bfb5f4bba3cfea048a18b3
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-10 23:00:48 -0700

    Task 102: Complete

--- 0x0F-python-network_0/102-catch_me.sh
+++ 0x0F-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-#get server to respond with "You Got Me!"
-curl  -L -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+# get server to respond with You Got Me
+curl -L -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+
