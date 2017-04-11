c992b551256596a9641a99dd80e3103d24760972
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-11 15:13:32 -0700

    Task 100: Had a stray debug statement

--- 0x10-python-network_1/100-github_commits.py
+++ 0x10-python-network_1/100-github_commits.py
@@ -10,7 +10,6 @@ if __name__ == "__main__":
     req = requests.get("https://api.github.com/repos/"
                        "{}/{}/commits".
                        format(argv[2], argv[1]))
-    print(req)
     for i in range(1, 11):
         print("{}: {}".format(
                 req.json()[i]['sha'],
