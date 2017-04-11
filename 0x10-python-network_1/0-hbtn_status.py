2ef280718aacdba86c6986707e08c3c2f2a2b4ca
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-11 13:14:56 -0700

    Task 0: Tried a new format string

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -9,7 +9,9 @@ if __name__ == "__main__":
     req = 'https://intranet.hbtn.io/status'
     with urllib.request.urlopen(req) as response:
         html = response.read()
-    print("""Body response:
-\t- type:  {}
-\t- content: {}
-\t- utf8 content: {}""".format(type(html), html, html.decode('utf-8')))
+    print("Body response:\n"
+          "\t- type:  {}\n"
+          "\t- content: {}\n"
+          "\t- utf8 content: {}".
+          format(type(html), html, html.decode('utf-8'))
+          )
