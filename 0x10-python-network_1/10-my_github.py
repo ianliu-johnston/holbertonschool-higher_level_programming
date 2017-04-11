20e23a259c940026b058b063aa1dae1b2f78d23d
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-11 15:59:41 -0700

    Task 10: Getting closer

--- 0x10-python-network_1/10-my_github.py
+++ 0x10-python-network_1/10-my_github.py
@@ -6,6 +6,6 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    requests.get("https://intranet.hbtn.co/auth/sign_in", auth=(argv[1], argv[2]))
+    requests.post("https://intranet.hbtn.co/auth/sign_in", headers={argv[1], argv[2]})
     req = requests.get("https://api.github.com/user", auth=(argv[1], argv[2]))
     print(req.json()['id'])
