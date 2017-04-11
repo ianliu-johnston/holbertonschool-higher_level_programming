768e456dd077b2a82dd2ba523fa5823328e79988
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-11 15:58:34 -0700

    Task 10: fixed url

--- 0x10-python-network_1/10-my_github.py
+++ 0x10-python-network_1/10-my_github.py
@@ -5,7 +5,7 @@ Basic Authentication with github api
 import requests
 from sys import argv
 
-requests.get("https://intranet.hbtn.co/auth/sign_in", auth=(argv[1], argv[2]))
 if __name__ == "__main__":
+    requests.get("https://intranet.hbtn.co/auth/sign_in", auth=(argv[1], argv[2]))
     req = requests.get("https://api.github.com/user", auth=(argv[1], argv[2]))
     print(req.json()['id'])
