7d5bc90f864ac25092b0491fff71640ec892c171
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-10 23:05:34 -0700

    Task 102: changed order

--- 0x0F-python-network_0/102-catch_me.sh
+++ 0x0F-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # get server to respond with You Got Me
-curl -s -L -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+curl -s -L -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me -X PUT
 
