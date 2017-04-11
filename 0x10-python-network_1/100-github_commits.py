481be13be26e30172cd67522caaddd4e30e85a33
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-11 15:12:17 -0700

    Task 100: Fixed style

--- 0x10-python-network_1/100-github_commits.py
+++ 0x10-python-network_1/100-github_commits.py
@@ -7,11 +7,12 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    req = requests.get("https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1]))
+    req = requests.get("https://api.github.com/repos/"
+                       "{}/{}/commits".
+                       format(argv[2], argv[1]))
     print(req)
-    for i in range(1,11):
+    for i in range(1, 11):
         print("{}: {}".format(
                 req.json()[i]['sha'],
                 req.json()[i]['commit']['author']['name']
             ))
-
