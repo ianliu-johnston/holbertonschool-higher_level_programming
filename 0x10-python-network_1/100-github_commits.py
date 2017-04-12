6b0f33d80fb576e11daf4288955d96c4a43c9ba8
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-12 13:03:47 -0700

    Previous only worked for >10 results, fixed

--- 0x10-python-network_1/100-github_commits.py
+++ 0x10-python-network_1/100-github_commits.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     req = requests.get("https://api.github.com/repos/"
                        "{}/{}/commits".
                        format(argv[2], argv[1]))
-    for i in range(1, 11):
+    for i in range(0, min(len(req.json()), 10)):
         print("{}: {}".format(
                 req.json()[i]['sha'],
                 req.json()[i]['commit']['author']['name']
