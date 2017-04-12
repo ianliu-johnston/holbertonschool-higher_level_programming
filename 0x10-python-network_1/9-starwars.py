0eb490a3725086fb05978d8bd85ca29b614b9ef5
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-11 18:27:30 -0700

    Task 9: Complete

--- 0x10-python-network_1/9-starwars.py
+++ 0x10-python-network_1/9-starwars.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+"""
+Sends a search request to the StarWars API
+"""
+import requests
+import sys
+
+
+if __name__=="__main__":
+    url = "https://swapi.co/api/people/"
+    req = requests.get(url, params={'search': sys.argv[1]})
+    names = []
+    for i in range(len(req.json()['results'])):
+        names.append(req.json()['results'][i]['name'])
+    print("Number of result: {:d}\n{}".format(i, "\n".join(names)))
