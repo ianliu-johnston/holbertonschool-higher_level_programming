e5adb4351a04cb9448d5a5bdc6b884379940a5dc
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-11 19:27:28 -0700

    Task 9: Added check for 0 results

--- 0x10-python-network_1/9-starwars.py
+++ 0x10-python-network_1/9-starwars.py
@@ -7,9 +7,12 @@ import sys
 
 
 if __name__ == "__main__":
+    names = []
+    i = 0
     url = "https://swapi.co/api/people/"
     req = requests.get(url, params={'search': sys.argv[1]})
-    names = []
     for i in range(len(req.json()['results'])):
         names.append(req.json()['results'][i]['name'])
-    print("Number of result: {:d}\n{}".format(i + 1, "\n".join(names)))
+    print("Number of result: {:d}\n{}".
+          format(i, "\n".join(names)),
+          end="" if i == 0 else "\n")
