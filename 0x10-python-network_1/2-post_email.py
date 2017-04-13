e5d2ea1954a3ee3b8067a0da97a23fdfc8d01a35
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-12 22:35:47 -0700

    Task 2: Complete

--- 0x10-python-network_1/2-post_email.py
+++ 0x10-python-network_1/2-post_email.py
@@ -2,12 +2,12 @@
 """
 Post some headers
 """
-import urllib
+import urllib.request
 import sys
 
 
 if __name__ == "__main__":
-    data = urllib.parse.urlencode({'email': argv[2]}).encode('utf-8')
+    data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('utf-8')
     req = urllib.request.Request(sys.argv[1])
     with urllib.request.urlopen(req) as response:
         print(response.read())
