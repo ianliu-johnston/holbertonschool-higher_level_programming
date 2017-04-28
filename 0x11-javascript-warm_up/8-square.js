2ae62101e2554d14ecc490637c79bc48987aea19
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-27 18:40:44 -0700

    Task 8: Complete

--- 0x11-javascript-warm_up/8-square.js
+++ 0x11-javascript-warm_up/8-square.js
@@ -1,1 +1,26 @@
 #!/usr/bin/node
+
+let i = 0, j = 0;
+const max = Number(process.argv[2]);
+
+if (max) {
+  while (i < max) {
+      console.log(Array(max + 1).join("X"));
+    i++;
+  }
+} else {
+  console.log('Missing size');
+}
+
+
+
+/*
+Write a script that prints a square
+
+The first argument is the size of the square
+If the first argument can't be converted to an integer, print "Missing size"
+You must use the character X to print the square
+You must use console.log(...) to print all output
+You are not allowed to use var
+You must use a loop (while, for, etc.)
+*/
