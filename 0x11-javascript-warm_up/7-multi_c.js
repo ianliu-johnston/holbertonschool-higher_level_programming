9f1f5a85dc86abe4ce692f7fe0e396fc5a5f2b6c
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-27 18:29:56 -0700

    Task 7: Complete

--- 0x11-javascript-warm_up/7-multi_c.js
+++ 0x11-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+let i = 0;
+const max = Number(process.argv[2]);
+
+if (max) {
+  while (i < max) {
+    console.log('C is fun');
+    i++;
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
