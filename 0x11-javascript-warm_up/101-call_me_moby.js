6dddb1462ecfce933d647a29e199c82a7437b864
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-27 21:31:08 -0700

    Changed function to anomyous function

--- 0x11-javascript-warm_up/101-call_me_moby.js
+++ 0x11-javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
-function callMeMoby (x, theFunction) {
+module.exports.callMeMoby = function (x, theFunction) {
   for (let i = 0; i < x; i++) {
     theFunction();
   }
 }
-module.exports.callMeMoby = callMeMoby;
