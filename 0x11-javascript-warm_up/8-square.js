fc212d4a85887f8280248f0b014d1f3d8701df52
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-27 20:10:32 -0700

    Task 8: Fixed style

--- 0x11-javascript-warm_up/8-square.js
+++ 0x11-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-let i = 0, j = 0;
+let i = 0;
 const max = Number(process.argv[2]);
 
 if (max) {
   while (i < max) {
-      console.log(Array(max + 1).join("X"));
+    console.log(Array(max + 1).join('X'));
     i++;
   }
 } else {
