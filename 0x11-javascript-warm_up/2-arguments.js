5b065a04224fcef4e6d4600650fe340a626fa464
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-27 20:21:10 -0700

    Task 2: One liner

--- 0x11-javascript-warm_up/2-arguments.js
+++ 0x11-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 
-console.log(process.argv.length < 3 ? 'No argument' : process.argv.length === 3 ? 'Argument' : 'Arguments' + ' found');
+console.log(process.argv.length < 3 ? 'No argument' : process.argv.length === 3 ? 'Argument found' : 'Arguments found');
