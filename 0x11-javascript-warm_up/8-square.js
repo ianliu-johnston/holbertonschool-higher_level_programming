dba923ab5f4f328150367004ee114489b4bbef50
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-27 19:19:27 -0700

    Task 8: Fixed style

--- 0x11-javascript-warm_up/8-square.js
+++ 0x11-javascript-warm_up/8-square.js
@@ -11,16 +11,3 @@ if (max) {
 } else {
   console.log('Missing size');
 }
-
-
-
-/*
-Write a script that prints a square
-
-The first argument is the size of the square
-If the first argument can't be converted to an integer, print "Missing size"
-You must use the character X to print the square
-You must use console.log(...) to print all output
-You are not allowed to use var
-You must use a loop (while, for, etc.)
-*/
