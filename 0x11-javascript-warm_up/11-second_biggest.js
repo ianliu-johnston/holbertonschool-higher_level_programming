badc94b2af5de0250f12560b3bd1f3a3d811379c
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-27 19:58:38 -0700

    Task 11: Fixed style

--- 0x11-javascript-warm_up/11-second_biggest.js
+++ 0x11-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-if (process.argv.length < 4){
+if (process.argv.length < 4) {
   console.log(0);
 } else {
-  list = process.argv.map(Number).sort(function(a,b){return a-b}); 
+  const list = process.argv.map(Number).sort(function (a, b) { return a - b; });
   console.log(list[list.length - 2]);
 }
