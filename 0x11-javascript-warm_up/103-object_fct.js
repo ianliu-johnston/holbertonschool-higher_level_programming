a2995735f60f52b36060e8b3e25c03be6db20f83
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-27 21:58:02 -0700

    Task 103: Missing semicolon

--- 0x11-javascript-warm_up/103-object_fct.js
+++ 0x11-javascript-warm_up/103-object_fct.js
@@ -4,7 +4,7 @@ let myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = function () { myObject.value += 1; }
+myObject.incr = function () { myObject.value += 1; };
 myObject.incr();
 console.log(myObject);
 myObject.incr();
