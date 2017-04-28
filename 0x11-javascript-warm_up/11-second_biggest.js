33ed585a737083103a32bb0cc595bc7b48abff71
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-27 20:09:18 -0700

    Task 11: One liner!

--- 0x11-javascript-warm_up/11-second_biggest.js
+++ 0x11-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
 
-if (process.argv.length < 4) {
-  console.log(0);
-} else {
-  const list = process.argv.map(Number).sort(function (a, b) { return a - b; });
-  console.log(list[list.length - 2]);
-}
+console.log(process.argv.length < 4 ? 0 : process.argv.map(Number).sort(function (a, b) { return a - b; })[process.argv.length - 2]);
