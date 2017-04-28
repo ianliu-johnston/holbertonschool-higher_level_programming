29ce67ef87730b75160e1c6b2b8372d5ae2d9f54
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-27 21:56:59 -0700

    Task 103: Complete

--- 0x11-javascript-warm_up/103-object_fct.js
+++ 0x11-javascript-warm_up/103-object_fct.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+let myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () { myObject.value += 1; }
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
