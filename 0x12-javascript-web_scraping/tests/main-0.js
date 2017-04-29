164ea174fb58c5256eeb9faf3b2ef64b8ac27677
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-29 15:41:38 -0700

    Adding tests

---
+++ 0x12-javascript-web_scraping/tests/main-0.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const Rectangle = require('./0-rectangle').Rectangle;
+
+const r1 = new Rectangle();
+console.log(r1);
+console.log(r1.constructor);
---
+++ 0x12-javascript-web_scraping/tests/main-3.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const Rectangle = require('./3-rectangle').Rectangle;
+
+const r1 = new Rectangle(2, 4);
+r1.print();
+
+const r2 = new Rectangle(6, 3);
+r2.print();
