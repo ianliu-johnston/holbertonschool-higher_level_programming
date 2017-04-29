0e9660d9420ca31a2e6699774b85a0413a138536
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-29 15:41:30 -0700

    Task 3: Complete

--- 0x12-javascript-web_scraping/3-rectangle.js
+++ 0x12-javascript-web_scraping/3-rectangle.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+module.exports.Rectangle = function Rectangle(w, h) {
+  if (Number(w) > 0 && Number(h) > 0){ 
+      this.width = w;
+      this.height = h;
+  }
+  this.print = function(){
+    for (var i = 0; i < h; i++)
+    {
+      console.log(Array(w + 1).join('X'));
+    }
+  };
+  return this;
+};
