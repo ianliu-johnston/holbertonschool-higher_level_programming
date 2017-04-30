4d06694e7a2dd8950973ed1c3824d6ad508a1d1c
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-30 11:09:37 -0700

    Rm comment

--- 0x12-javascript-web_scraping/4-rectangle.js
+++ 0x12-javascript-web_scraping/4-rectangle.js
@@ -9,15 +9,13 @@ module.exports.Rectangle = function Rectangle (w, h) {
     console.log(Array(this.size + 1).join(Array(this.size + 1).join('X') + '\n').slice(0, -1));
   };
   this.double = function () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
+    this.width *= 2;
+    this.height *= 2;
   };
   this.rotate = function () {
     const tmp = this.width;
     this.width = this.height;
     this.height = tmp;
-//    return(Rectangle(this.height, this.width));
-//    ^---This returns a copy of the rectangle without mutating the original
   };
   return this;
 };
