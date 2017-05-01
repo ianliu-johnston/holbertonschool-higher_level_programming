fb9349d5f7c7e185ad80bca29a42f58cfc1f2b54
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-30 22:27:13 -0700

    Task 5: Cannot figure out what the checker wants

--- 0x12-javascript-web_scraping/5-square.js
+++ 0x12-javascript-web_scraping/5-square.js
@@ -4,8 +4,6 @@ const Rectangle = require('./4-rectangle.js').Rectangle;
 
 function Square (size) {
   Rectangle.call(this, size, size);
-  this.size = size;
-  return this;
 }
 
 module.exports.Square = Square;
