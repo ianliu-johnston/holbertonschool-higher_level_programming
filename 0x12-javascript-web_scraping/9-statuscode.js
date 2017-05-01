a16c6a75cf2733702ae42b56ffe0151882740192
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-30 22:50:55 -0700

    Task 9: Bare basics

--- 0x12-javascript-web_scraping/9-statuscode.js
+++ 0x12-javascript-web_scraping/9-statuscode.js
@@ -1,1 +1,5 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+request({ method: 'GET', uri: process.argv[2], json: true }, (err, response, body) => console.log("code: " + response.statusCode));
