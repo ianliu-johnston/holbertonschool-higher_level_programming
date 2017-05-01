6be50f890f618a53c7981c3442e032e0b27cbdcf
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-30 22:21:55 -0700

    Reorder

--- 0x12-javascript-web_scraping/5-square.js
+++ 0x12-javascript-web_scraping/5-square.js
@@ -3,7 +3,7 @@
 const Rectangle = require('./4-rectangle.js').Rectangle;
 
 module.exports.Square = function (size) {
-  Rectangle.call(this, size, size);
   this.size = size;
+  Rectangle.call(this, size, size);
   return this;
 };
