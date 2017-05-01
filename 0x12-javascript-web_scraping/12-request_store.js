111b39c05a3865a92cf39ff74a168c2f56652096
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-30 23:03:21 -0700

    Task 12: Complete

--- 0x12-javascript-web_scraping/12-request_store.js
+++ 0x12-javascript-web_scraping/12-request_store.js
@@ -1,1 +1,6 @@
 #!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+request({ method: 'GET', uri: process.argv[2], json: true }, 'utf-8', (err, response, body) => !err ? fs.appendFile(process.argv[3], body) : console.log(err));
