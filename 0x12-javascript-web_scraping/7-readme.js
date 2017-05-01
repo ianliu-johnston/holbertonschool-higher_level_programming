253f4c2f77528a2320e452277e6fc0e44ccbb729
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-30 22:32:08 -0700

    Task 8: Complete

--- 0x12-javascript-web_scraping/7-readme.js
+++ 0x12-javascript-web_scraping/7-readme.js
@@ -2,7 +2,8 @@
 
 const fs = require('fs');
 
-if (process.argv[2])
-fs.readFile(process.argv[2], 'utf-8', function (file, err) {
-  err ? console.log(err) : console.log(file);
-});
+if (process.argv[2]) {
+  fs.readFile(process.argv[2], 'utf-8', function (file, err) {
+    err ? console.log(err) : console.log(file);
+  });
+}
--- 0x12-javascript-web_scraping/8-writeme.js
+++ 0x12-javascript-web_scraping/8-writeme.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+
+const fs = require('fs');
+
+const file = process.argv[2];
+const string = process.argv[3];
+
+fs.appendFile(file, string, 'utf-8', err => err ? console.log(err) : '');
