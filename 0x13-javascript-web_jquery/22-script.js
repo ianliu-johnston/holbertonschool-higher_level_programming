2d981c6b2b183151a941751f260ec4ffffcb33f9
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-05-02 15:19:13 -0700

    Task 22, 23: Complete

--- 0x13-javascript-web_jquery/22-script.js
+++ 0x13-javascript-web_jquery/22-script.js
@@ -1,1 +1,7 @@
-#!/ usr/bin/node
+$(document).ready(function(){
+  $.get('http://swapi.co/api/films?format=json', function(data){
+    const results = eval(data).results;
+    for (var key in results)
+      $("#list_movies").append("<LI>" + results[key].title+ "</LI>");
+  });
+});
