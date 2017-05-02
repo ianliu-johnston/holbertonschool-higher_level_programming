997707c0f7cfc644ef334459612998e2e8bf4508
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-05-02 15:35:27 -0700

    Test

--- 0x13-javascript-web_jquery/22-script.js
+++ 0x13-javascript-web_jquery/22-script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   $.get('http://swapi.co/api/films?format=json', function (data) {
+  const d = eval(data);
     for (var key in data.results) { $('#list_movies').append('<LI>' + data.results[key].title + '</LI>'); }
   });
 });
