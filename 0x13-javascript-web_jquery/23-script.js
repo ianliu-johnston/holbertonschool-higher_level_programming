d23dbf192f3eff247a222726355b9dd1e14a3236
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-05-02 15:28:04 -0700

    Task 23: Complete

--- 0x13-javascript-web_jquery/23-script.js
+++ 0x13-javascript-web_jquery/23-script.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-  $.get('https://query.yahooapis.com/v1/public/yql?q=select%20wind%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22San%20Francisco%2C%20CA%22)&format=json', function(data){
-  $("#sf_wind_speed").text(data.query.results.channel.wind.speed);
+$(document).ready(function () {
+  $.get('https://query.yahooapis.com/v1/public/yql?q=select%20wind%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22San%20Francisco%2C%20CA%22)&format=json', function (data) {
+    $('#sf_wind_speed').text(data.query.results.channel.wind.speed);
   });
 });
