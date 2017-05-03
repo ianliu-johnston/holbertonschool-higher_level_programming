7a56a6cbc33608ba0fa48338c9d10ba33961e5f6
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-05-02 19:43:40 -0700

    Task 101: Fixed style

--- 0x13-javascript-web_jquery/101-script.js
+++ 0x13-javascript-web_jquery/101-script.js
@@ -1,14 +1,14 @@
-$(document).ready(function(){
+$(document).ready(function () {
   let counter = 0;
-  $('#add_item').click(function(){
+  $('#add_item').click(function () {
     counter += 1;
     $('.my_list').append('<LI>Item</LI>');
   });
-  $('#remove_item').click(function(){
+  $('#remove_item').click(function () {
     $('.my_list li').eq(counter).remove();
     counter -= 1;
   });
-  $('#clear_list').click(function(){
+  $('#clear_list').click(function () {
     counter = 0;
     $('.my_list').empty();
   });
