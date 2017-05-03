faa6c81be00d8197fd1a56189e11caf857e57f15
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-05-02 19:44:15 -0700

    Task 102: Complete

--- 0x13-javascript-web_jquery/102-script.js
+++ 0x13-javascript-web_jquery/102-script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-    $("#btn_search").click(function(){
-      $.get('https://query.yahooapis.com/v1/public/yql?q=select%20wind%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22:' + encodeURIComponent($("#city_search").val()) + '%22)&format=json', function(data) {
-      $("#wind_speed").text(data.query.results.channel.wind.speed);
+  $('#btn_search').click(function () {
+    $.get('https://query.yahooapis.com/v1/public/yql?q=select%20wind%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22:' + encodeURIComponent($('#city_search').val()) + '%22)&format=json', function (data) {
+      $('#wind_speed').text(data.query.results.channel.wind.speed);
+    });
   });
- });
 });
