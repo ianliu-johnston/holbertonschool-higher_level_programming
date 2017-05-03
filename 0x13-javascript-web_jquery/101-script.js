501d70ddbb9f8d452f8fe5d4af1841fd49ecfc4a
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-05-02 18:19:10 -0700

    Task 101: Complete

--- 0x13-javascript-web_jquery/101-script.js
+++ 0x13-javascript-web_jquery/101-script.js
@@ -1,1 +1,14 @@
-#!/ usr/bin/node
+$(document).ready(function(){
+  let counter = 0;
+  $('#add_item').click(function(){
+    $('.my_list').append('<LI>Item' + counter + '</LI>');
+  });
+  $('#remove_item').click(function(){
+    $('.my_list li').eq(counter).remove();
+    counter -= 1;
+  });
+  $('#clear_list').click(function(){
+    counter = 0;
+    $('.my_list').empty();
+  });
+});
