29d3912eec5de71c1d4eb09199f8783d915660a2
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-05-02 17:02:01 -0700

    Moving all main html into tests folder.

---
+++ 0x13-javascript-web_jquery/tests/node.test.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const list2 = ["hala", "haha", "lala", "laha"];
+
+for (let item in list2) {
+  console.log(list2[item]);
+}
+
+for (let i in list2){
+  console.log(i);
+}
---
+++ 0x13-javascript-web_jquery/tests/py.test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+list2 = ["hala", "haha", "lala", "laha"]
+
+for item in list2:
+    print(item)
+
+for index, item in enumerate(list2):
+    print(index)
